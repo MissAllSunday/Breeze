38b2e9ab4895fa64c699e2873ca9ff634d1da363
MissAllSunday <missallsunday@simplemachines.org>
2012-08-04 15:40:18 -0500

    Need to associate the raw name with the actual names somehow...
    Signed-off-by: MissAllSunday <missallsunday@simplemachines.org>

--- Sources/Breeze/BreezeMention.php
+++ Sources/Breeze/BreezeMention.php
@@ -44,11 +44,12 @@ class BreezeMention
 	protected $_settings;
 	protected $_tools;
 	protected $_string;
-	protected $_regex = '~{([\s\w,;-_\[\]\\\/\+\.\~\$\!]+)}~u';
+	protected $_regex;
 	protected $_query;
 
 	function __construct()
 	{
+		$this->_regex = '~{([\s\w,;-_\[\]\\\/\+\.\~\$\!]+)}~u';
 	}
 
 	public function mention($string, $noti_info = false)
@@ -60,83 +61,84 @@ class BreezeMention
 			return false;
 
 		$this->_string = $string;
+		$queryNames = array();
 
 		/* Search for all possible names */
 		if (preg_match_all($this->_regex, $this->_string, $matches, PREG_SET_ORDER))
 			foreach($matches as $m)
-				$querynames[] = trim($m[1]);
+				$queryNames[] = trim($m[1]);
 
-		/* Nothing was found */
-		else
-			return $this->_string;
-
-		/* Load and set what we need */
-		$this->_query = Breeze::quickQuery('members');
-		$this->_notification = Breeze::notifications();
-		$this->_settings = Breeze::settings();
-		$this->_tools = Breeze::tools();
-		$searchNames = array();
-
-		/* Sorry, you just can't tag a single person more than once */
-		$queryNames = array_unique($queryNames);
-
-		/* Don't abuse... sorry, hardcoded for now*/
-		if (count($queryNames) >= 10)
-			$queryNames = array_slice($queryNames, 0, 10);
-
-		/* Let's make a quick query here... */
-		$tempParams = array (
-			'rows' => 'id_member, member_name, real_name',
-			'where' => 'real_name IN({array_string:names}) OR member_name IN({array_string:names})',
-		);
-		$tempData = array(
-			'names' => $querynames,
-		);
-		$this->_query->params($tempParams, $tempData);
-		$this->_query->getData('id_member', false);
-
-		/* Get the actual users */
-		$searchNames = !is_array($this->_query->dataResult()) ? array($this->_query->dataResult()) : $this->_query->dataResult();
-
-		/* We got some results */
-		if (!empty($searchNames))
+		/* Do this if we have something */
+		if (!empty($queryNames))
 		{
-			/* You can't notify yourself */
-			if (array_key_exists($user_info['id'], $searchNames))
-				unset($searchNames[$user_info['id']]);
+			/* Load and set what we need */
+			$this->_query = Breeze::quickQuery('members');
+			$this->_notification = Breeze::notifications();
+			$this->_settings = Breeze::settings();
+			$this->_tools = Breeze::tools();
+			$searchNames = array();
 
-			/* Lets create the notification */
-			foreach ($searchNames as $name)
+			/* We need an array */
+			$queryNames = is_array($queryNames) ? $queryNames : array($queryNames);
+
+			/* Sorry, you just can't tag a single person more than once */
+			$queryNames = array_unique($queryNames);
+
+			/* Don't abuse... sorry, hardcoded for now */
+			if (count($queryNames) >= 10)
+				$queryNames = array_slice($queryNames, 0, 10);
+
+			/* Let's make a quick query here... */
+			$tempParams = array (
+				'rows' => 'id_member, member_name, real_name',
+				'where' => 'real_name IN({array_string:names}) OR member_name IN({array_string:names})',
+			);
+			$tempData = array(
+				'names' => $queryNames,
+			);
+			$this->_query->params($tempParams, $tempData);
+			$this->_query->getData('id_member', false);
+
+			/* Get the actual users */
+			$searchNames = !is_array($this->_query->dataResult()) ? array($this->_query->dataResult()) : $this->_query->dataResult();
+
+			/* We got some results */
+			if (!empty($searchNames))
 			{
-				$params = array(
-					'user' => $user_info['id'],
-					'user_to' => $name['id_member'],
-					'type' => 'mention',
-					'time' => time(),
-					'read' => 0,
-					'content' => $noti_info,
-				);
+				/* You can't notify yourself */
+				if (array_key_exists($user_info['id'], $searchNames))
+					unset($searchNames[$user_info['id']]);
 
-				/* Notification here */
-				$this->_notification->createMention($params);
-
-				/* Ugly but necessary to include both display and real name */
-				foreach ($querynames as $query)
+				/* Let's create the notification */
+				foreach ($searchNames as $name)
 				{
-					$key = array_search($query, $name)
+					/* Ugly, but we need to associate the raw name with the actual names somehow... */
+					foreach ($queryNames as $query)
+						if (in_array($query, $name))
+							$name['raw_name'] = $query;
 
-					$find[] = '{'. $name['member_name'] .'}';
+					$params = array(
+						'user' => $user_info['id'],
+						'user_to' => $name['id_member'],
+						'type' => 'mention',
+						'time' => time(),
+						'read' => 0,
+						'content' => $noti_info,
+					);
+
+					/* Notification here */
+					$this->_notification->createMention($params);
+
+					/* Let's create the preformat */
+					$find[] = '{'. $name['raw_name'] .'}';
+					$replace[] = '{'. $name['id_member'] .','. $name['member_name'] .','. $name['real_name'] .'}';
 				}
-
-				/* Building the pre-format, format is as follows: {id,name,display} */
-				$replace[] = '{'. $name['id_member'] .','. $name['member_name'] .','. $name['real_name'] .'}';
 			}
+
+			/* Finally do the replacement */
+			$this->_string = str_replace($find, $replace, $this->_string);
 		}
 
-		/* Finally do the replacement */
-		$this->_string = str_replace($find, $replace, $this->_string);
-
-		/* Return the string */
 		return $this->_string;
 	}
 }
