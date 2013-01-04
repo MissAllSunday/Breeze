06fb51df7dab5883c97eec3f431dfa14b9a8c8ac
Suki <suki@missallsunday.com>
2013-01-04 16:39:53 -0600

    Inject dependencies on BreezeForm()
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeForm.php
+++ Sources/Breeze/BreezeForm.php
@@ -38,182 +38,182 @@
 if (!defined('SMF'))
 	die('No direct access...');
 
-	class BreezeForm
+class BreezeForm
+{
+	public $method;
+	public $action;
+	public $name;
+	public $id_css;
+	public $class;
+	public $elements = array();
+	public $status = 0;
+	public $buffer = '';
+	public $onsubmit;
+	public $text;
+
+	function __construct($text)
 	{
-		public $method;
-		public $action;
-		public $name;
-		public $id_css;
-		public $class;
-		public $elements = array();
-		public $status = 0;
-		public $buffer = '';
-		public $onsubmit;
-		public $text;
-
-		function __construct($form = array())
-		{
-			$this->text = Breeze::text();
-		}
-
-		public function returnElementNames()
-		{
-			$this->returnelementsnames = array();
-
-			if (!empty($this->elements))
-				foreach ($this->elements as $e)
-					if (isset($e['name']) && !empty($e['name']))
-						$this->returnelementsnames[$e['name']] = $e['name'];
-
-			return $this->returnelementsnames;
-		}
-
-		private function addElement($element)
-		{
-			$plus = $this->countElements();
-			$element['id'] = $this->countElements();
-			$this->elements[$element['id']] = $element;
-		}
-
-		private function countElements()
-		{
-			return count($this->elements);
-		}
-
-		private function getElement($id)
-		{
-			return $this->elements[$id];
-		}
-
-		private function getNextElement()
-		{
-			if( $this->status == $this->countElements())
-				$this->status = 0;
-
-			$element = $this->getElement($this->status);
-			$this->status++;
-		}
-
-		function addSelect($name, $text, $values = array())
-		{
-			$element['type'] = 'select';
-			$element['name'] = $name;
-			$element['values'] = $values;
-			$element['text']  = $text;
-			$element['html_start'] = '<'. $element['type'] .' name="default_options['. $element['name'] .']">';
-			$element['html_end'] = '</'. $element['type'] .'>';
-
-			foreach($values as $k => $v)
-				$element['values'][$k] = '<option value="' .$k. '" '. (isset($v[1]) && $v[1] == 'selected' ? 'selected="selected"' : '') .'>'. $this->text->getText('user_settings_'. $v[0]) .'</option>';
-
-			return $this->addElement($element);
-		}
-
-		function addCheckBox($name, $text, $checked = false)
-		{
-			$element['type'] = 'checkbox';
-			$element['name'] = $name;
-			$element['value'] = 1;
-			$element['checked'] = empty($checked) ? '' : 'checked="checked"';
-			$element['text'] = $text;
-			$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="default_options['. $element['name'] .']" value="'. (int)$element['value'] .'" '. $element['checked'] .' class="input_check" />';
-
-			return $this->addElement($element);
-		}
-
-		function addText($name, $text, $value, $size = false, $maxlength = false)
-		{
-			$element['type'] = 'text';
-			$element['name'] = $name;
-			$element['value'] = $value;
-			$element['text'] = $text;
-			$element['size'] = empty($size) ? 'size="20"' : 'size="' .$size. '"';
-			$element['maxlength'] = empty($maxlength) ? 'maxlength="20"' : 'maxlength="' .$maxlength. '"';
-			$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="'. $element['name'] .'" value="'. $element['value'] .'" '. $element['size'] .' '. $element['maxlength'] .' class="input_text" />';
-
-			return $this->addElement($element);
-		}
-
-		function addTextArea($name, $text, $value)
-		{
-			$element['type'] = 'textarea';
-			$element['name'] = $name;
-			$element['value'] = empty($value) ? '' : $value;
-			$element['text'] = $text;
-			$element['html'] = '<'. $element['type'] .' name="default_options['. $element['name'] .']" id="'. $element['name'] .'">'. $element['value'] .'</'. $element['type'] .'>';
-
-			return $this->addElement($element);
-		}
-
-		function addHiddenField($name, $value)
-		{
-			$element['type'] = 'hidden';
-			$element['name'] = $name;
-			$element['value'] = $value;
-			$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="'. $element['name'] .'" value="'. $element['value'] .'" />';
-
-			return $this->addElement($element);
-		}
-
-		function addHr()
-		{
-			$element['type'] = 'hr';
-			$element['html'] = '<hr />';
-
-			return $this->addElement($element);
-		}
-
-		function display()
-		{
-			$this->buffer .= '<dl class="settings">';
-			$element = $this->getNextElement();
-
-			foreach($this->elements as $el)
-			{
-				switch($el['type'])
-				{
-					case 'textarea':
-					case 'checkbox':
-					case 'text':
-						$this->buffer .= '<dt>
-							<span style="font-weight:bold;">'. $this->text->getText('user_settings_'. $el['text']) .'</span>
-							<br /><span class="smalltext">'. $this->text->getText('user_settings_'. $el['text'] .'_sub') .'</span>
-						</dt>
-						<dd>
-							<input type="hidden" name="default_options['. $el['name'] .']" value="0" />'. $el['html'] .'
-						</dd>';
-						break;
-					case 'select':
-						$this->buffer .= '<dt>
-							<span style="font-weight:bold;">'. $this->text->getText('user_settings_'. $el['text']) .'</span>
-							<br /><span class="smalltext">'. $this->text->getText('user_settings_'.$el['text'] .'_sub') .'</span>
-						</dt>
-						<dd>
-							<input type="hidden" name="default_options['. $el['name'] .']" value="0" />'. $el['html_start'] .'';
-
-						foreach($el['values'] as $k => $v)
-							$this->buffer .= $v .'';
-
-						$this->buffer .= $el['html_end'] .'
-						</dd>';
-						break;
-					case 'hidden':
-					case 'submit':
-						$this->buffer .= '<dt></dt>
-						<dd>
-							'. $el['html'] .'
-						</dd>';
-						break;
-					case 'hr':
-						$this->buffer .= '</dl>
-							'. $el['html'] .'
-						<dl class="settings">';
-						break;
-				}
-			}
-
-			$this->buffer .= '</dl>';
-
-			return $this->buffer;
-		}
+		$this->text = $text;
 	}
+
+	public function returnElementNames()
+	{
+		$this->returnelementsnames = array();
+
+		if (!empty($this->elements))
+			foreach ($this->elements as $e)
+				if (isset($e['name']) && !empty($e['name']))
+					$this->returnelementsnames[$e['name']] = $e['name'];
+
+		return $this->returnelementsnames;
+	}
+
+	private function addElement($element)
+	{
+		$plus = $this->countElements();
+		$element['id'] = $this->countElements();
+		$this->elements[$element['id']] = $element;
+	}
+
+	private function countElements()
+	{
+		return count($this->elements);
+	}
+
+	private function getElement($id)
+	{
+		return $this->elements[$id];
+	}
+
+	private function getNextElement()
+	{
+		if( $this->status == $this->countElements())
+			$this->status = 0;
+
+		$element = $this->getElement($this->status);
+		$this->status++;
+	}
+
+	function addSelect($name, $text, $values = array())
+	{
+		$element['type'] = 'select';
+		$element['name'] = $name;
+		$element['values'] = $values;
+		$element['text']  = $text;
+		$element['html_start'] = '<'. $element['type'] .' name="default_options['. $element['name'] .']">';
+		$element['html_end'] = '</'. $element['type'] .'>';
+
+		foreach($values as $k => $v)
+			$element['values'][$k] = '<option value="' .$k. '" '. (isset($v[1]) && $v[1] == 'selected' ? 'selected="selected"' : '') .'>'. $this->text->getText('user_settings_'. $v[0]) .'</option>';
+
+		return $this->addElement($element);
+	}
+
+	function addCheckBox($name, $text, $checked = false)
+	{
+		$element['type'] = 'checkbox';
+		$element['name'] = $name;
+		$element['value'] = 1;
+		$element['checked'] = empty($checked) ? '' : 'checked="checked"';
+		$element['text'] = $text;
+		$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="default_options['. $element['name'] .']" value="'. (int)$element['value'] .'" '. $element['checked'] .' class="input_check" />';
+
+		return $this->addElement($element);
+	}
+
+	function addText($name, $text, $value, $size = false, $maxlength = false)
+	{
+		$element['type'] = 'text';
+		$element['name'] = $name;
+		$element['value'] = $value;
+		$element['text'] = $text;
+		$element['size'] = empty($size) ? 'size="20"' : 'size="' .$size. '"';
+		$element['maxlength'] = empty($maxlength) ? 'maxlength="20"' : 'maxlength="' .$maxlength. '"';
+		$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="'. $element['name'] .'" value="'. $element['value'] .'" '. $element['size'] .' '. $element['maxlength'] .' class="input_text" />';
+
+		return $this->addElement($element);
+	}
+
+	function addTextArea($name, $text, $value)
+	{
+		$element['type'] = 'textarea';
+		$element['name'] = $name;
+		$element['value'] = empty($value) ? '' : $value;
+		$element['text'] = $text;
+		$element['html'] = '<'. $element['type'] .' name="default_options['. $element['name'] .']" id="'. $element['name'] .'">'. $element['value'] .'</'. $element['type'] .'>';
+
+		return $this->addElement($element);
+	}
+
+	function addHiddenField($name, $value)
+	{
+		$element['type'] = 'hidden';
+		$element['name'] = $name;
+		$element['value'] = $value;
+		$element['html'] = '<input type="'. $element['type'] .'" name="default_options['. $element['name'] .']" id="'. $element['name'] .'" value="'. $element['value'] .'" />';
+
+		return $this->addElement($element);
+	}
+
+	function addHr()
+	{
+		$element['type'] = 'hr';
+		$element['html'] = '<hr />';
+
+		return $this->addElement($element);
+	}
+
+	function display()
+	{
+		$this->buffer .= '<dl class="settings">';
+		$element = $this->getNextElement();
+
+		foreach($this->elements as $el)
+		{
+			switch($el['type'])
+			{
+				case 'textarea':
+				case 'checkbox':
+				case 'text':
+					$this->buffer .= '<dt>
+						<span style="font-weight:bold;">'. $this->text->getText('user_settings_'. $el['text']) .'</span>
+						<br /><span class="smalltext">'. $this->text->getText('user_settings_'. $el['text'] .'_sub') .'</span>
+					</dt>
+					<dd>
+						<input type="hidden" name="default_options['. $el['name'] .']" value="0" />'. $el['html'] .'
+					</dd>';
+					break;
+				case 'select':
+					$this->buffer .= '<dt>
+						<span style="font-weight:bold;">'. $this->text->getText('user_settings_'. $el['text']) .'</span>
+						<br /><span class="smalltext">'. $this->text->getText('user_settings_'.$el['text'] .'_sub') .'</span>
+					</dt>
+					<dd>
+						<input type="hidden" name="default_options['. $el['name'] .']" value="0" />'. $el['html_start'] .'';
+
+					foreach($el['values'] as $k => $v)
+						$this->buffer .= $v .'';
+
+					$this->buffer .= $el['html_end'] .'
+					</dd>';
+					break;
+				case 'hidden':
+				case 'submit':
+					$this->buffer .= '<dt></dt>
+					<dd>
+						'. $el['html'] .'
+					</dd>';
+					break;
+				case 'hr':
+					$this->buffer .= '</dl>
+						'. $el['html'] .'
+					<dl class="settings">';
+					break;
+			}
+		}
+
+		$this->buffer .= '</dl>';
+
+		return $this->buffer;
+	}
+}
