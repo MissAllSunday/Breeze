121ec26d3f7f2810623932a8d09e7290b2f25cd5
Suki <suki@missallsunday.com>
2013-01-11 14:18:04 -0600

    This is 1.0 Beta 3!!
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/BreezeAdmin.template.php
+++ Themes/default/BreezeAdmin.template.php
@@ -5,7 +5,7 @@
  *
  * The purpose of this file is to show the admin section for the mod's settings
  * @package Breeze mod
- * @version 1.0 Beta 2
+ * @version 1.0 Beta 3
  * @author Jessica González <missallsunday@simplemachines.org>
  * @copyright Copyright (c) 2012, Jessica González
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
