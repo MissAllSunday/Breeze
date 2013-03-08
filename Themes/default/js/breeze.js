1e1f9a63a3b3df0f0120a077fcca72cab934a822
Suki <suki@missallsunday.com>
2013-03-07 18:03:01 -0600

    Scroll to top
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/js/breeze.js
+++ Themes/default/js/breeze.js
@@ -456,3 +456,34 @@ jQuery(document).ready(function(){
 		});
 	});
 });
+
+/* Scroll to Top */
+jQuery(document).ready(function(){
+
+	// hide #back-top first
+	jQuery("#back-top").hide();
+
+	jQuery(function ()
+	{
+		jQuery(window).scroll(function ()
+		{
+			if (jQuery(this).scrollTop() > 100)
+			{
+				jQuery('#back-top').fadeIn();
+			}
+			else
+			{
+				jQuery('#back-top').fadeOut();
+			}
+		});
+
+		jQuery('#back-top a').click(function ()
+		{
+			jQuery('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
