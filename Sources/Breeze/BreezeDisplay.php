e35c9319b6b510f551f932663c1eba38d17667a8
Suki <suki@missallsunday.com>
2013-03-21 09:52:31 -0600

    Replace all properties with context vars
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeDisplay.php
+++ Sources/Breeze/BreezeDisplay.php
@@ -68,7 +68,7 @@ class BreezeDisplay
 
 	public function HTML()
 	{
-		global $scripturl, $user_info, $context;
+		global context;
 
 		// Load the user info
 		$this->tools->loadUserInfo($this->params['poster_id']);
@@ -82,5 +82,7 @@ class BreezeDisplay
 		$context['sub_template'] = 'main';
 		$context['Breeze']['type'] = $this->type;
 		$context['Breeze']['params'] = $this->params;
+		$context['Breeze']['permissions'] = $this->permissions
+		$context['Breeze']['text'] = $this->text;
 	}
 }
--- Themes/default/BreezeDisplay.template.php
+++ Themes/default/BreezeDisplay.template.php
@@ -37,81 +37,81 @@
 
 function template_main()
 {
-	global $txt, $context;
+	global $scripturl, $user_info, $context;
 
-		switch ($this->type)
-		{
-			case 'status':
-				$return = '
-		<li class="windowbg" id ="status_id_'. $this->params['id'] .'">
-			<span class="topslice">
-				<span></span>
-			</span>
-			<div class="breeze_user_inner">
-				<div class="breeze_user_status_avatar">
-					'. $context['Breeze']['user_info'][$this->params['poster_id']]['facebox'] .'
-				</div>
-				<div class="breeze_user_status_comment">
-					'. $this->params['body'] .'
-					<div class="breeze_options"><span class="time_elapsed">'. $this->params['time'] .' </span>';
+	switch ($context['Breeze']['type'])
+	{
+		case 'status':
+			$return = '
+	<li class="windowbg" id ="status_id_'. $context['Breeze']['params']['id'] .'">
+		<span class="topslice">
+			<span></span>
+		</span>
+		<div class="breeze_user_inner">
+			<div class="breeze_user_status_avatar">
+				'. $context['Breeze']['user_info'][$context['Breeze']['params']['poster_id']]['facebox'] .'
+			</div>
+			<div class="breeze_user_status_comment">
+				'. $context['Breeze']['params']['body'] .'
+				<div class="breeze_options"><span class="time_elapsed">'. $context['Breeze']['params']['time'] .' </span>';
 
-					// Delete link
-					if ($this->permissions['deleteStatus'])
-						$return .= '| <a href="javascript:void(0)" id="'. $this->params['id'] .'" class="breeze_delete_status">'. $this->text->getText('general_delete') .'</a>';
+				// Delete link
+				if ($context['Breeze']['permissions']['deleteStatus'])
+					$return .= '| <a href="javascript:void(0)" id="'. $context['Breeze']['params']['id'] .'" class="breeze_delete_status">'. $context['Breeze']['text']->getText('general_delete') .'</a>';
 
-					$return .= '</div>
-					<hr />
-					<div id="comment_flash_'. $this->params['id'] .'"></div>';
+				$return .= '</div>
+				<hr />
+				<div id="comment_flash_'. $context['Breeze']['params']['id'] .'"></div>';
 
-					$return .= '<ul class="breeze_comments_list" id="comment_loadplace_'. $this->params['id'] .'">';
+				$return .= '<ul class="breeze_comments_list" id="comment_loadplace_'. $context['Breeze']['params']['id'] .'">';
 
-						// New status don't have comments...
+					// New status don't have comments...
 
-						// display the new comments ^o^
+					// display the new comments ^o^
+					$return .= '
+					<li id="breeze_load_image_comment_'. $context['Breeze']['params']['id'] .'" style="margin:auto; text-align:center;"></li>';
+
+					// Close the list
+					$return .= '</ul>';
+
+					// display the form for new comments
+					if ($context['Breeze']['permissions']['postcomments'])
 						$return .= '
-						<li id="breeze_load_image_comment_'. $this->params['id'] .'" style="margin:auto; text-align:center;"></li>';
-
-						// Close the list
-						$return .= '</ul>';
-
-						// display the form for new comments
-						if ($this->permissions['postcomments'])
-							$return .= '
-							<span><form action="'. $scripturl. '?action=breezeajax;sa=postcomment" method="post" name="formID_'. $this->params['id'] .'" id="formID_'. $this->params['id'] .'">
-								<textarea id="textboxcontent_'. $this->params['id'] .'" cols="40" rows="2"></textarea>
-								<input type="hidden" value="'. $this->params['poster_id'] .'" name="status_owner_id'. $this->params['id'] .'" id="status_owner_id'. $this->params['id'] .'" />
-								<input type="hidden" value="'. $this->params['owner_id'] .'" name="profile_owner_id'. $this->params['id'] .'" id="profile_owner_id'. $this->params['id'] .'" />
-								<input type="hidden" value="'. $this->params['id'] .'" name="status_id'. $this->params['id'] .'" id="status_id'. $this->params['id'] .'" />
-								<input type="hidden" value="'. $user_info['id'] .'" name="poster_comment_id'. $this->params['id'] .'" id="poster_comment_id'. $this->params['id'] .'" /><br />
-								<input type="submit" value="Comment" class="comment_submit" id="'. $this->params['id'] .'" />
-							</form></span>';
+						<span><form action="'. $scripturl. '?action=breezeajax;sa=postcomment" method="post" name="formID_'. $context['Breeze']['params']['id'] .'" id="formID_'. $context['Breeze']['params']['id'] .'">
+							<textarea id="textboxcontent_'. $context['Breeze']['params']['id'] .'" cols="40" rows="2"></textarea>
+							<input type="hidden" value="'. $context['Breeze']['params']['poster_id'] .'" name="status_owner_id'. $context['Breeze']['params']['id'] .'" id="status_owner_id'. $context['Breeze']['params']['id'] .'" />
+							<input type="hidden" value="'. $context['Breeze']['params']['owner_id'] .'" name="profile_owner_id'. $context['Breeze']['params']['id'] .'" id="profile_owner_id'. $context['Breeze']['params']['id'] .'" />
+							<input type="hidden" value="'. $context['Breeze']['params']['id'] .'" name="status_id'. $context['Breeze']['params']['id'] .'" id="status_id'. $context['Breeze']['params']['id'] .'" />
+							<input type="hidden" value="'. $user_info['id'] .'" name="poster_comment_id'. $context['Breeze']['params']['id'] .'" id="poster_comment_id'. $context['Breeze']['params']['id'] .'" /><br />
+							<input type="submit" value="Comment" class="comment_submit" id="'. $context['Breeze']['params']['id'] .'" />
+						</form></span>';
 
 
-					// Close the div
-					$return .= '</div>
+				// Close the div
+				$return .= '</div>
+				<div class="clear"></div>
+			</div>
+		<span class="botslice">
+			<span></span>
+		</span>
+		</li>';
+			break;
+		case 'comment':
+			$return = '
+				<li class="windowbg2" id ="comment_id_'. $context['Breeze']['params']['id'] .'">
+					<div class="breeze_user_comment_avatar">
+						'. $context['Breeze']['user_info'][$context['Breeze']['params']['poster_id']]['facebox'] .'<br />
+					</div>
+					<div class="breeze_user_comment_comment">
+						'. $context['Breeze']['params']['body'] .'
+						<div class="breeze_options">
+							<span class="time_elapsed">'. $context['Breeze']['params']['time'] .'</span> | <a href="javascript:void(0)" id="'. $context['Breeze']['params']['id'] .'" class="breeze_delete_comment">Delete</a>
+						</div>
+					</div>
 					<div class="clear"></div>
-				</div>
-			<span class="botslice">
-				<span></span>
-			</span>
-			</li>';
-				break;
-			case 'comment':
-				$return = '
-					<li class="windowbg2" id ="comment_id_'. $this->params['id'] .'">
-						<div class="breeze_user_comment_avatar">
-							'. $context['Breeze']['user_info'][$this->params['poster_id']]['facebox'] .'<br />
-						</div>
-						<div class="breeze_user_comment_comment">
-							'. $this->params['body'] .'
-							<div class="breeze_options">
-								<span class="time_elapsed">'. $this->params['time'] .'</span> | <a href="javascript:void(0)" id="'. $this->params['id'] .'" class="breeze_delete_comment">Delete</a>
-							</div>
-						</div>
-						<div class="clear"></div>
-					</li>';
-				break;
-		}
+				</li>';
+			break;
+	}
 
-		return $return;
+	return $return;
 }
