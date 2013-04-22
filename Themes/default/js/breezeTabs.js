5958014dae83c3cec4031c24de7e54f60eec0ee9
Suki <suki@missallsunday.com>
2013-04-22 10:23:48 -0500

    Yet another file!
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Themes/default/js/breezeTabs.js
@@ -0,0 +1,34 @@
+/**
+ * breezeTabs.js
+ *
+ * @package Breeze mod
+ * @version 1.0 Beta 3
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013 Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is http://missallsunday.com code.
+ *
+ * The Initial Developer of the Original Code is
+ * Jessica González.
+ * Portions created by the Initial Developer are Copyright (c) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ */
+
