016ae4659e2850c5e5df5b270b397914851cb4dc
Suki <suki@missallsunday.com>
2013-07-01 18:16:05 -0500

    default avatar
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeUserInfo.php
+++ Sources/Breeze/BreezeUserInfo.php
@@ -77,7 +77,7 @@ class BreezeUserInfo
 			$context['Breeze']['user_info'][$user['id']]['name'] = $user['name'];
 
 		// It all starts with the user's avatar or username...
-		$context['Breeze']['user_info'][$user['id']]['facebox'] .= (!empty($user['avatar']['href']) ? '<a href="#facebox_'. $user['id'] .'" rel="facebox"><img src="'.$user['avatar']['href'].'" width="50px" /></a>' : $user['link']);
+		$context['Breeze']['user_info'][$user['id']]['facebox'] .= '<a href="#facebox_'. $user['id'] .'" rel="facebox"><img src="'.(!empty($user['avatar']['href']) ? ''.$user['avatar']['href'].'' : $settings['default_theme_url'] .'/images/breeze/default_user.png') . '" width="50px" /></a>';
 
 		// Set the data
 		$context['Breeze']['user_info'][$user['id']]['data'] = '
