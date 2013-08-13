0d08a1860531c3f37506d34ec6395b262e94e788
Suki <suki@missallsunday.com>
2013-08-13 11:37:01 -0500

    A bunch of globals and also lets overheat the server...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeGeneral.php
+++ Sources/Breeze/BreezeGeneral.php
@@ -39,84 +39,89 @@ if (!defined('SMF'))
 	die('No direct access...');
 
 
-	// Get the latest entries of your buddies
-	function generalWall()
+// Get the latest entries of your buddies
+function generalWall()
+{
+	global $txt, $scripturl, $context, $memberContext, $sourcedir;
+	global $modSettings,  $user_info, $breezeController;
+
+	loadtemplate(Breeze::$name);
+	writeLog(true);
+
+	// Time to overheat the server...
+	if (empty($breezeController))
+		$breezeController = new BreezeController();
+
+	// Set all the page stuff
+	$context['page_title'] = 'demo';
+	$context['sub_template'] = 'general_wall';
+	$context['linktree'][] = array(
+		'url' => $scripturl . '?action=wall',
+		'name' => 'demo'
+	);
+
+	// Headers
+	BreezeTools::headers(true);
+}
+
+// Show a single status with all it's comments
+function singleStatus()
+{
+	global $user_info, $scripturl, $context, $memberContext;
+
+	// Load all we need
+	loadtemplate(Breeze::$name);
+
+	// Prepare all we need
+	$globals = new BreezeGlobals('get');
+	$tools = BreezeSettings::getInstance();
+	$query = BreezeQuery::getInstance();
+	$status = array();
+	$status['comments'] = array();
+
+	// Headers
+	BreezeTools::headers();
+
+	// Set all the page stuff
+	$context['sub_template'] = 'singleStatus';
+	$context['page_title'] = $tools->getText('singleStatus_pageTitle');
+	$context['canonical_url'] = $scripturl . '?action=wall;sa=singlestatus';
+
+	// The visitor's permissions
+	$context['Breeze']['visitor']['post_status'] = allowedTo('breeze_postStatus');
+	$context['Breeze']['visitor']['post_comment'] = allowedTo('breeze_postComments');
+	$context['Breeze']['visitor']['delete_status_comments'] = allowedTo('breeze_deleteStatus');
+
+	// get the status data
+	if ($globals->validate('statusID'))
+		$status = $query->GetStatusByID($globals->getValue('statusID'));
+
+	// If no ID is set, then load the lastest status
+	else
+		$status = $query->GetStatusByLast();
+
+	// Get tue user id
+	$users_to_load[] = $status['owner_id'];
+
+	// Load the corresponding comments
+	$status['comments'] = $query->GetCommentsByStatus($status['id']);
+
+	// Get the id for all the users who commented on the status
+	foreach($status['comments'] as $c)
+		$users_to_load[] = $c['poster_id'];
+
+	// We have all the IDs, let's prune the array a little
+	$new_temp_array = array_unique($users_to_load);
+
+	// Load the data
+	loadMemberData($new_temp_array, false, 'profile');
+	foreach($new_temp_array as $u)
 	{
-		global $txt, $scripturl, $context;
-
-		loadtemplate(Breeze::$name);
-		writeLog(true);
-
-		// Set all the page stuff
-		$context['page_title'] = 'demo';
-		$context['sub_template'] = 'general_wall';
-		$context['linktree'][] = array(
-			'url' => $scripturl . '?action=wall',
-			'name' => 'demo'
-		);
-
-		// Headers
-		BreezeTools::headers(true);
+		loadMemberContext($u);
+		$user = $memberContext[$u];
+		$context['Breeze']['user_info'][$user['id']] = BreezeUserInfo::Profile($user);
 	}
 
-	// Show a single status with all it's comments
-	function singleStatus()
-	{
-		global $user_info, $scripturl, $context, $memberContext;
-
-		// Load all we need
-		loadtemplate(Breeze::$name);
-
-		// Prepare all we need
-		$globals = new BreezeGlobals('get');
-		$tools = BreezeSettings::getInstance();
-		$query = BreezeQuery::getInstance();
-		$status = array();
-		$status['comments'] = array();
-
-		// Headers
-		BreezeTools::headers();
-
-		// Set all the page stuff
-		$context['sub_template'] = 'singleStatus';
-		$context['page_title'] = $tools->getText('singleStatus_pageTitle');
-		$context['canonical_url'] = $scripturl . '?action=wall;sa=singlestatus';
-
-		// The visitor's permissions
-		$context['Breeze']['visitor']['post_status'] = allowedTo('breeze_postStatus');
-		$context['Breeze']['visitor']['post_comment'] = allowedTo('breeze_postComments');
-		$context['Breeze']['visitor']['delete_status_comments'] = allowedTo('breeze_deleteStatus');
-
-		// get the status data
-		if ($globals->validate('statusID'))
-			$status = $query->GetStatusByID($globals->getValue('statusID'));
-
-		// If no ID is set, then load the lastest status
-		else
-			$status = $query->GetStatusByLast();
-
-		// Get tue user id
-		$users_to_load[] = $status['owner_id'];
-
-		// Load the corresponding comments
-		$status['comments'] = $query->GetCommentsByStatus($status['id']);
-
-		// Get the id for all the users who commented on the status
-		foreach($status['comments'] as $c)
-			$users_to_load[] = $c['poster_id'];
-
-		// We have all the IDs, let's prune the array a little
-		$new_temp_array = array_unique($users_to_load);
-
-		// Load the data
-		loadMemberData($new_temp_array, false, 'profile');
-		foreach($new_temp_array as $u)
-		{
-			loadMemberContext($u);
-			$user = $memberContext[$u];
-			$context['Breeze']['user_info'][$user['id']] = BreezeUserInfo::Profile($user);
-		}
-
-		// Send the data to the template
-		$context['Breeze']['singleStatus'] = $status;
-	}
+	// Send the data to the template
+	$context['Breeze']['singleStatus'] = $status;
+}
