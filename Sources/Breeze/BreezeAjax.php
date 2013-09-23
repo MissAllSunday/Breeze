79fcb86ff49887941d495b1974cce0ed9911e9c9
Suki <suki@missallsunday.com>
2013-09-22 19:13:18 -0500

    Set a dummy time_raw var
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeAjax.php
+++ Sources/Breeze/BreezeAjax.php
@@ -161,6 +161,7 @@ class BreezeAjax
 
 			// Store the status
 			$params['id'] = $this->_query->insertStatus($params);
+			$parmas['time_raw'] = time();
 
 			// All went good or so it seems...
 			if (!empty($params['id']))
@@ -256,6 +257,7 @@ class BreezeAjax
 
 			// Store the comment
 			$params['id'] = $this->_query->insertComment($params);
+			$parmas['time_raw'] = time();
 
 			// The Comment was inserted
 			if (!empty($params['id']))
