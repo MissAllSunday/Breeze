a167561352f4e2d47c819b977f359461b2b9492d
Suki <suki@missallsunday.com>
2013-12-16 10:05:28 -0600

    re-write how notifications appear Fixes #84 Its now all handle by the client via breezeNoti.js
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/js/breezeNoti.js
+++ Themes/default/js/breezeNoti.js
@@ -63,10 +63,10 @@ breezeNotifications.stream = function(currentUser)
 					layout: 'topRight',
 					closeWith: ['button'],
 					buttons: [
-						{addClass: 'button_submit', text: breeze_noti_markasread, onClick: function($noty) {
+						{addClass: 'button_submit', text: breeze_noti_markasread, onClick: function($noty){
 							jQuery.ajax({
 								type: 'POST',
-								url: smf_scripturl + '?action=breezeajax;sa=notimark;js=1',
+								url: smf_scripturl + '?action=breezeajax;sa=notimark;js=1;' + window.breeze_session_var + '=' + window.breeze_session_id,
 								data: ({content : noti.id, user : noti.user}),
 								cache: false,
 								dataType: 'json',
@@ -81,22 +81,20 @@ breezeNotifications.stream = function(currentUser)
 									}
 								},
 								error: function (html){
-
 										noty({text: breeze_error_message, timeout: 3500, type: 'error'});
 								},
 							});
 
 								$noty.close();
 						}},
-						{addClass: 'button_submit', text: breeze_noti_delete, onClick: function($noty) {
+						{addClass: 'button_submit', text: breeze_noti_delete, onClick: function($noty){
 							jQuery.ajax({
 								type: 'POST',
-								url: smf_scripturl + '?action=breezeajax;sa=notidelete;js=1',
+								url: smf_scripturl + '?action=breezeajax;sa=notidelete;js=1;' + window.breeze_session_var + '=' + window.breeze_session_id,
 								data: ({content : noti.id, user : noti.user}),
 								cache: false,
 								dataType: 'json',
-								success: function(html)
-								{
+								success: function(html){
 									if(html.type == 'error'){
 										noty({text: html.data, timeout: 3500, type: 'error'});
 									}
@@ -109,19 +107,33 @@ breezeNotifications.stream = function(currentUser)
 										noty({text: html.data, timeout: 3500, type: 'success'});
 									}
 								},
-								error: function (html)
-								{
+								error: function (html){
 										noty({text: breeze_error_message, timeout: 3500, type: 'error'});
 								},
 							});
 								$noty.close();
 						}},
-						{addClass: 'button_submit', text: breeze_noti_cancel, onClick: function($noty) {
+						{addClass: 'button_submit', text: breeze_noti_cancel, onClick: function($noty){
 								$noty.close();
 						}}
 					]
 				});
 			});
+
+			// Show a close all button
+			noty({
+				text: breeze_noti_closeAll,
+				type: 'warning',
+				dismissQueue: true,
+				layout: 'topRight',
+				closeWith: ['click'],
+				callback: {
+					afterClose: function() {
+							jQuery.noty.closeAll();
+					},
+				onShow: function() {window.setTimeout("jQuery.noty.closeAll()", 5 * 1000 );},
+				},
+					});
 
 			// Append the number of notifications to the wall button
 			jQuery('#button_wall  a.firstlevel span').append(' ['+ number +']');
