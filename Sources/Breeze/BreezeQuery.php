d4fc333aca8e78264a8350939a4f2f002db669cf
Suki <suki@missallsunday.com>
2014-01-31 18:42:39 -0600

    Shorter license header
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeQuery.php
+++ Sources/Breeze/BreezeQuery.php
@@ -3,37 +3,12 @@
 /**
  * BreezeQuery
  *
- * The purpose of this file is to have all queries made by this mod in a single place, probably the most important file and the biggest one too.
  * @package Breeze mod
  * @version 1.0
  * @author Jessica González <suki@missallsunday.com>
  * @copyright Copyright (c) 2011, 2014 Jessica González
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
-
-/*
-* Version: MPL 1.1
-*
-* The contents of this file are subject to the Mozilla Public License Version
-* 1.1 (the "License"); you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS" basis,
-* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-* for the specific language governing rights and limitations under the
-* License.
-*
-* The Original Code is http://missallsunday.com code.
-*
-* The Initial Developer of the Original Code is
-* Jessica González.
-* Portions created by the Initial Developer are Copyright (c) 2012
-* the Initial Developer. All Rights Reserved.
-*
-* Contributor(s):
-*
-*/
 
 if (!defined('SMF'))
 	die('No direct access...');
--- Sources/Breeze/BreezeUserInfo.php
+++ Sources/Breeze/BreezeUserInfo.php
@@ -3,7 +3,6 @@
 /**
  * BreezeUserInfo
  *
- * The purpose of this file is to generate a div with the user common info, name, avatar, post, icons, stuff like that.
  * @package Breeze mod
  * @version 1.0
  * @author Jessica González <suki@missallsunday.com>
@@ -11,35 +10,9 @@
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 
-/*
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is http://missallsunday.com code.
- *
- * The Initial Developer of the Original Code is
- * Jessica González.
- * Portions created by the Initial Developer are Copyright (c) 2012
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- */
-
 if (!defined('SMF'))
 	die('No direct access...');
 
-	/* The purpose of this file is to centralize the user info,
-	helps to avoid having to write code over and over again */
 class BreezeUserInfo
 {
 	public static function profile($u)
