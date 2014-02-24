f8202ee3f6084df76883a94c9b9a54ae62217885
Suki <suki@missallsunday.com>
2014-02-24 17:36:11 -0600

    Short header on functions template file
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/BreezeFunctions.template.php
+++ Themes/default/BreezeFunctions.template.php
@@ -1,38 +1,13 @@
 <?php
 
 /**
- * BreezeFunctions.template
+ * BreezeFunctions.template.php
  *
- * The purpose of this file is to modularize some of the most used blocks of code, the point is to reduce view code and maximize and re-use as much code as possible
  * @package Breeze mod
  * @version 1.0
  * @author Jessica Gonzalez <suki@missallsunday.com>
- * @copyright Copyright (c) 2011, 2014 Jessica Gonzalez
+ * @copyright Copyright (c) 2011, 2014, Jessica Gonzalez
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
- */
-
-/*
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is http://missallsunday.com code.
- *
- * The Initial Developer of the Original Code is
- * Jessica Gonzalez.
- * Portions created by the Initial Developer are Copyright (c) 2012
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
  */
 
 function breeze_status($data, $returnVar = false)
