d5f080197b4010eb2fcde8091fb5f03f26aa9826
Suki <suki@missallsunday.com>
2014-02-24 18:39:52 -0600

    Add the per user checking to the per user wall
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/Breeze.php
+++ Sources/Breeze/Breeze.php
@@ -155,15 +155,16 @@ class Breeze
 			// We need your settings...
 			$userSettings = $breezeController->get('query')->getUserSettings($user_info['id']);
 
-			$profile_areas['info']['areas']['summary'] = array(
-				'label' => $tools->text('general_wall'),
-				'file' => Breeze::$folder . 'BreezeUser.php',
-				'function' => 'breezeWall',
-				'permission' => array(
-					'own' => 'profile_view_own',
-					'any' => 'profile_view_any',
-					),
-				);
+			if (!empty($userSettings['wall']))
+				$profile_areas['info']['areas']['summary'] = array(
+					'label' => $tools->text('general_wall'),
+					'file' => Breeze::$folder . 'BreezeUser.php',
+					'function' => 'breezeWall',
+					'permission' => array(
+						'own' => 'profile_view_own',
+						'any' => 'profile_view_any',
+						),
+					);
 
 			// If the mod is enable, then create another page for the default profile page
 			$profile_areas['info']['areas']['static'] = array(
