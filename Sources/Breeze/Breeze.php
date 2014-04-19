81d9a9c36159125f73997edc8e01531070e37ec5
Suki <suki@missallsunday.com>
2014-04-19 10:46:03 -0500

    we're here
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/Breeze.php
+++ Sources/Breeze/Breeze.php
@@ -499,7 +499,7 @@ class Breeze extends Pimple
 				$context['html_headers'] .= '
 		breeze.text.'. $var .' = '. JavaScriptEscape($tools->text($var));
 
-				// Since where here already, load the current User (currentSettings) object
+				// Since we're here already, load the current User (currentSettings) object
 				foreach (Breeze::$allSettings as $k)
 					$context['html_headers'] .= '
 		breeze.currentSettings.'. $k .' = '. (isset($userSettings[$k]) ? (is_array($userSettings[$k]) ? json_encode($userSettings[$k]) : JavaScriptEscape($userSettings[$k])) : 'false') .';';
