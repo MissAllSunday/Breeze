a162e660714d4bdf3268206e747b4cb4b798eb32
Suki <suki@missallsunday.com>
2014-04-26 08:48:46 -0500

    I suppose the username is a more common choice for showing in a link.
    Signed-off-by: Suki <suki@missallsunday.com>
    
    Conflicts:
    	Sources/Breeze/BreezeQuery.php
    
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeQuery.php
+++ Sources/Breeze/BreezeQuery.php
@@ -1510,8 +1510,7 @@ class BreezeQuery
 			return false;
 
 		// Arrays only please!
-		$users = (array) $users;
-		$users = array_unique($users);
+		$users = array_unique((array) $users);
 		$toLoad = array();
 		$returnData = array();
 		$toCache = array();
@@ -1530,8 +1529,13 @@ class BreezeQuery
 					'name' => $profile['real_name'],
 					'id' => $profile['id_member'],
 					'href' => $this->scripturl . '?action=profile;u=' . $profile['id_member'],
+<<<<<<< HEAD
 					'link' => '<a href="' . $this->scripturl . '?action=profile;u=' . $profile['id_member'] . '" title="' . $txt['profile_of'] . ' ' . $profile['real_name'] . '">' . $profile['real_name'] . '</a>',
 					'gender' => $profile['gender'],
+=======
+					'link' => '<a href="' . $this->scripturl . '?action=profile;u=' . $profile['id_member'] . '" title="' . $txt['profile_of'] . ' ' . $profile['real_name'] . '">' . $profile['member_name'] . '</a>',
+					// 'gender' => $profile['gender'],
+>>>>>>> e8d7480... I suppose the username is a more common choice for showing in a link.
 				);
 
 				unset($profile);
