200b30234cd5b2477a9eb16383e1d714ff732681
Suki <suki@missallsunday.com>
2014-05-04 19:34:39 -0500

    Use my correct name
    Signed-off-by: Suki <suki@missallsunday.com>

--- remove.php
+++ remove.php
@@ -5,8 +5,8 @@
  *
  * @package Breeze mod
  * @version 1.0
- * @author Jessica Gonzalez <suki@missallsunday.com>
- * @copyright Copyright (c) 2011, 2014, Jessica Gonz�lez
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2011, 2014, Jessica González
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 
