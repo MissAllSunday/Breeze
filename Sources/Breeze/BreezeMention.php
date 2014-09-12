af9b1a2b7410c1a5b2d991490d28e8e88a6246c2
Suki <suki@missallsunday.com>
2014-09-12 11:51:37 -0500

    And more stuff removed...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeMention.php
+++ Sources/Breeze/BreezeMention.php
@@ -5,7 +5,7 @@
  *
  * @package Breeze mod
  * @version 1.1
- * @author Jessica GonzÃ¡lez <suki@missallsunday.com>
+ * @author Jessica González <suki@missallsunday.com>
  * @copyright Copyright (c) 2011, 2014 Jessica González
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
