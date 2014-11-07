a1d3129029317c53ae2e1ad431d1af6207f8e9cc
Suki <suki@missallsunday.com>
2014-11-06 18:07:08 -0600

    Some small getters and setters
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeTools.php
+++ Sources/Breeze/BreezeTools.php
@@ -387,4 +387,27 @@ class BreezeTools
 
 		return $perm;
 	}
+
+	public function setResponse($message, $type)
+	{
+		if (empty($message) || empty($type))
+			return;
+
+		// Yeah, a nice session var...
+		$_SESSION['Breeze']['response'] = array(
+			'message' => $message,
+			'type' => $type;
+		);
+	}
+
+	public function getResponse()
+	{
+		if (empty($_SESSION['Breeze']['response']))
+			return false;
+
+		$response = $_SESSION['Breeze']['response'];
+		unset($_SESSION['Breeze']['response']);
+
+		return $response;
+	}
 }
