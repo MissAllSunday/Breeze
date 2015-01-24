47f077f174fef2fc1f1ffb449a5778a741747e8b
Suki <suki@missallsunday.com>
2015-01-24 10:52:29 -0600

    And a brand new file/class
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/Breeze/BreezeTrackActions.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * BreezeTrackActions
+ *
+ * @package Breeze mod
+ * @version 1.1
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2011, 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class BreezeTrackActions extends Breeze
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+}
