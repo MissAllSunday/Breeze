e17e9265e4e7579c45e0cd700307f89918076696
Suki <suki@missallsunday.com>
2015-03-10 19:53:55 -0600

    breezeLoadMore totally untested code, of course it will work wonderfully so no need to actually test this at all no sr.. no need...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/scripts/breeze/breezeLoadMore.js
+++ Themes/default/scripts/breeze/breezeLoadMore.js
@@ -3,87 +3,100 @@
  @license http://www.mozilla.org/MPL/MPL-1.1.html
 */
 
-$(function() {
+var breezeLoadMore = function(oOptions) {
+	this.options = oOptions;
 
-	// Fires up the load more thingy
-	if (breeze.currentSettings.load_more){
+	this.numberOfEvents = 0;
 
-		var numberOfEvents = 0;
+	// Magic!
+	this.showMoarButton();
+};
 
-		// Hide the normal pagination.
-		$('.pagesection').hide();
+breezeLoadMore.prototype.showMoarButton()
+{
+	// Don't show anything if there isn't enough items to display... this actually should never happen but better be safe...
+	if (this.options.pagination.totalItems <= this.options.pagination.maxIndex)
+		return false;
 
-		showMoarButton();
+	// Replacing any pagination div?
+	if (this.options.paginationClass)
+		$(this.options.paginationClass).hide();
+
+	// The rest is pretty straight forward...
+	$('<button/>', {
+		id: this.options.button.id,
+		text: this.options.button.text,
+		click: this.clickButton()
+	}).appendTo(this.options.button.appendTo);
+}
+
+breezeLoadMore.prototype.clickButton()
+{
+	// Add one to the number of iterations, pretty important!
+	this.numberOfEvents++;
+
+	// Need to keep a reference...
+	var = oObject = this;
+
+	$('<ul/>', {
+		id: 'tempDIV_'+ this.numberOfEvents,
+		class: this.options.target.css,
+		style: ''
+	}).appendTo(this.options.target.appendTo).hide();
+
+	// Let us pass some data to the server.
+	passingData = {
+		numberTimes : this.numberOfEvents,
+		comingFrom : breeze.tools.comingFrom,
+};
+	// And fill the rest with options.pagination object.
+	$.extend(passingData, this.options.pagination);
+
+	$.ajax({
+		// Send the data and oh boy there's a lot of it!
+		url: smf_scripturl + '?action=breezeajax;sa=fetch;js=1;' + smf_session_var + '=' + smf_session_id,
+		data : passingData,
+		cache: false,
+		dataType: 'json',
+		success: function(html)
+		{
+			oObject.onSuccess(html);
+		},
+		error: function (html){
+			breeze.tools.showNoti(html);
+		}
+	});
+}
+
+breezeLoadMore.prototype.onSuccess(html)
+{
+	// The server response as a JSON object
+	if(html.type == 'info'){
+
+		// Append the html to our temp DIV.
+		if (html.data != 'end'){
+
+			// Create a unique UL for this very own status, isn't it great?
+			$('#tempDIV_'+ this.numberOfEvents).append(html.data).fadeIn('slow', 'linear', function(){});
+		}
+
+		// No more data:(
+		else{
+			noty({
+				text: this.options.endText,
+				timeout: 3500,
+				theme: 'relax',
+				type: 'success'
+			});
+			$('#loadMoar').fadeOut('slow');
+			return;
+		}
 	}
 
-	function showMoarButton(){
-
-	// Don't show anything if there isn't enough items to display...
-	// if (breeze.pagination.totalItems <= breeze.pagination.maxIndex)
-		// return false;
-
-		// Add a nice button...
-		$('<button/>', {
-			id: 'loadMoar',
-			text: breeze.text.load_more,
-			click: function () {
-				numberOfEvents++;
-
-				$('<ul/>', {
-					id: 'tempDIV_'+ numberOfEvents,
-					class: 'breeze_status',
-					style: ''
-				}).appendTo('#breezeAppendTo').hide();
-
-				// Append some more data to breeze.pagination.
-				breeze.pagination.numberTimes = numberOfEvents;
-				breeze.pagination.comingFrom = breeze.tools.comingFrom;
-
-				$.ajax({
-					// Send the data and oh boy there's a lot of it!
-					url: smf_scripturl + '?action=breezeajax;sa=fetch;js=1;' + smf_session_var + '=' + smf_session_id,
-					data : breeze.pagination,
-					cache: false,
-					dataType: 'json',
-					success: function(html)
-					{
-						// The server response as a JSON object
-						if(html.type == 'info'){
-
-							// Append the html to our temp DIV.
-							if (html.data != 'end'){
-
-								// Create a unique UL for this very own status, isn't it great?
-								$('#tempDIV_'+ numberOfEvents).append(html.data).fadeIn('slow', 'linear', function(){});
-							}
-
-							// No more data:(
-							else{
-								noty({
-									text: breeze.text.page_loading_end,
-									timeout: 3500,
-									theme: 'relax',
-									type: 'success'
-								});
-								$('#loadMoar').fadeOut('slow');
-								return;
-							}
-						}
-
-						else if(html.type == 'error'){
-							breeze.tools.showNoti(html);
-						}
-					},
-					error: function (html){
-						breeze.tools.showNoti(html);
-					}
-				});
-			}
-		}).appendTo('#tab-wall');
+	else if(html.type == 'error'){
+		breeze.tools.showNoti(html);
 	}
-});
-
-
+}
 
 
 
