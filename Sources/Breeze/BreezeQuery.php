9dd78867afa1199d6c72a1f2f740ac92dd0899ce
Suki <suki@missallsunday.com>
2015-04-04 16:11:34 -0600

    Gotta show the correct entitties
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeQuery.php
+++ Sources/Breeze/BreezeQuery.php
@@ -1584,7 +1584,7 @@ class BreezeQuery
 				'poster_id' => $row['status_poster_id'],
 				'time' => $this->_app['tools']->timeElapsed($row['status_time']),
 				'time_raw' => $row['status_time'],
-				'body' => parse_bbc($row['status_body']),
+				'body' => un_htmlspecialchars(parse_bbc($row['status_body'])),
 				'comments' => array(),
 				'canHas' => $this->_app['tools']->permissions('Status', $row['status_owner_id'], $row['status_poster_id']),
 				'likes' => array(),
@@ -1609,7 +1609,7 @@ class BreezeQuery
 				'profile_id' => $row['comments_profile_id'],
 				'time' => $this->_app['tools']->timeElapsed($row['comments_time']),
 				'time_raw' => $row['comments_time'],
-				'body' => parse_bbc($row['comments_body']),
+				'body' => un_htmlspecialchars(parse_bbc($row['comments_body'])),
 				'canHas' => $this->_app['tools']->permissions('Comments', $row['comments_profile_id'], $row['comments_poster_id']),
 				'likes' => array(),
 			);
