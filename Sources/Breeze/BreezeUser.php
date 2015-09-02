fc8e72b7e67d6e58a97eba3de626972033ff83d6
Suki <suki@missallsunday.com>
2015-09-01 19:04:41 -0500

    Add some more event listeners and remove the bootstrap stuff
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Breeze/BreezeUser.php
+++ Sources/Breeze/BreezeUser.php
@@ -627,26 +627,15 @@ class BreezeUser extends Breeze
 		$form->addHTML(array(
 			'name' => 'cover_select',
 			'html' => '
-<div class="dropzone" id="coverUpload"></div>
-	<div id="actions" class="row">
-		<div class="col-lg-7">
-		<!-- The fileinput-button span is used to style the file input field as button -->
-		<span class="btn btn-success fileinput-button">
-			<i class="glyphicon glyphicon-plus"></i>
-			<span>Add files...</span>
-		</span>
-		<button type="submit" class="btn btn-primary start">
-			<i class="glyphicon glyphicon-upload"></i>
-			<span>Start upload</span>
-		</button>
-		<button type="reset" class="btn btn-warning cancel">
-			<i class="glyphicon glyphicon-ban-circle"></i>
-			<span>Cancel upload</span>
-		</button>
+	<div id="coverUpload"></div>
+	<div id="actions">
+		<div>
+			<a class="button_submit cancel">Cancel upload</a>
+			<a class="button_submit start">Start upload</a>
+			<a class="button_submit fileinput-button">Add files...</a>
 		</div>
 
-		<div class="col-lg-5">
-		<!-- The global file processing state -->
+		<div>
 		<span class="fileupload-process">
 			<div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
 			<div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
@@ -654,35 +643,28 @@ class BreezeUser extends Breeze
 		</span>
 		</div>
 	</div>
-<div class="table table-striped" class="files" id="previews">
-	<div id="template" class="file-row">
-		<!-- This is used as the file preview template -->
-		<div>
+	<div class="files" id="previews">
+		<div id="template">
+			<div>
 				<span class="preview"><img data-dz-thumbnail /></span>
-		</div>
-		<div>
+			</div>
+			<div>
 				<p class="name" data-dz-name></p>
-				<strong class="error text-danger" data-dz-errormessage></strong>
-		</div>
-		<div>
+				<strong class="error" data-dz-errormessage></strong>
+			</div>
+			<div>
 				<p class="size" data-dz-size></p>
 				<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
 					<div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
 				</div>
+			</div>
+			<div>
+				<a class="button_submit start">Start</a>
+				<a data-dz-remove class="button_submit cancel">Cancel</a>
+				<a data-dz-remove class="button_submit delete">Delete</a>
+			</div>
 		</div>
-		<div>
-			<button class="btn btn-primary start">
-				<span>Start</span>
-			</button>
-			<button data-dz-remove class="btn btn-warning cancel">
-				<span>Cancel</span>
-			</button>
-			<button data-dz-remove class="btn btn-danger delete">
-				<span>Delete</span>
-			</button>
-		</div>
-	</div>
-</div>'
+	</div>'
 		));
 
 		// Send the form to the template
--- Themes/default/scripts/breeze/coverUpload.js
+++ Themes/default/scripts/breeze/coverUpload.js
@@ -2,55 +2,75 @@
  Copyright (c) 2011, 2015, Jessica González
  @license http://www.mozilla.org/MPL/ MPL 2.0
 */
-var previewNode = document.querySelector('#template');
-previewNode.id = '';
-var previewTemplate = previewNode.parentNode.innerHTML;
-previewNode.parentNode.removeChild(previewNode);
+$(function() {
+	var previewNode = document.querySelector('#template');
+	previewNode.id = '';
+	var previewTemplate = previewNode.parentNode.innerHTML;
+	previewNode.parentNode.removeChild(previewNode);
 
-var myDropzone = new Dropzone('div#coverUpload', {
-	url: _coverUpload.url,
-	maxFilesize: 1,
-	uploadMultiple:false,
-	maxFiles:1,
-	acceptedFiles: 'jpg,gif',
-	thumbnailWidth: 80,
-	thumbnailHeight: 80,
-	parallelUploads: 20,
-	previewTemplate: previewTemplate,
-	autoQueue: false,
-	previewsContainer: '#previews',
-	clickable: '.fileinput-button',
-	dictDefaultMessage: 'Drop files here to upload',
+	var myDropzone = new Dropzone('div#coverUpload', {
+		url: _coverUpload.url,
+		maxFilesize: 1,
+		uploadMultiple:false,
+		maxFiles:1,
+		acceptedFiles: 'image/*',
+		thumbnailWidth: 80,
+		thumbnailHeight: 80,
+		parallelUploads: 20,
+		previewTemplate: previewTemplate,
+		autoQueue: false,
+		previewsContainer: '#previews',
+		clickable: '.fileinput-button',
+		dictDefaultMessage: 'Drop files here to upload',
+		paramName: 'files',
+	});
+
+	myDropzone.on('addedfile', function(file) {
+
+		// Hookup the start button
+		file.previewElement.querySelector('.start').onclick = function() { myDropzone.enqueueFile(file); };
+	});
+
+	// Update the total progress bar
+	myDropzone.on('totaluploadprogress', function(progress) {
+		document.querySelector('#total-progress .progress-bar').style.width = progress + '%';
+	});
+
+	myDropzone.on('error', function(file, errorMessage, xhr) {
+		// Remove the "start" button.
+		file.previewElement.querySelector('.start').remove();
+
+		// Set a nice css class to make it more obvious theres an error.
+		file.previewElement.setAttribute("class", "errorbox");
+	});
+
+	myDropzone.on('success', function(file, responseText, e) {
+		// Do whatever we need to do with the server response.
+		file.previewElement.setAttribute("class", "" + responseText.type +"box");
+
+		// If there wasn't any error, change the current cover.
+		if (responseText.type == 'info'){
+			document.querySelector('.current_cover').src = _coverUpload.imgsrc;
+		}
+	});
+
+	myDropzone.on('sending', function(file) {
+		// Show the total progress bar when upload starts
+		document.querySelector('#total-progress').style.opacity = '1';
+		// And disable the start button
+		file.previewElement.querySelector('.start').setAttribute('disabled', 'disabled');
+	});
+
+	// Hide the total progress bar when nothing's uploading anymore
+	myDropzone.on('queuecomplete', function(progress) {
+		document.querySelector('#total-progress').style.opacity = '0';
+	});
+
+	// The upload and cancel all buttons.
+	document.querySelector('#actions .start').onclick = function() {
+		myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+	};
+	document.querySelector('#actions .cancel').onclick = function() {
+		myDropzone.removeAllFiles(true);
+	};
 });
-
-myDropzone.on('addedfile', function(file) {
-  // Hookup the start button
-  file.previewElement.querySelector('.start').onclick = function() { myDropzone.enqueueFile(file); };
-});
-
-// Update the total progress bar
-myDropzone.on('totaluploadprogress', function(progress) {
-  document.querySelector('#total-progress .progress-bar').style.width = progress + '%';
-});
-
-myDropzone.on('sending', function(file) {
-  // Show the total progress bar when upload starts
-  document.querySelector('#total-progress').style.opacity = '1';
-  // And disable the start button
-  file.previewElement.querySelector('.start').setAttribute('disabled', 'disabled');
-});
-
-// Hide the total progress bar when nothing's uploading anymore
-myDropzone.on('queuecomplete', function(progress) {
-  document.querySelector('#total-progress').style.opacity = '0';
-});
-
-// Setup the buttons for all transfers
-// The 'add files' button doesn't need to be setup because the config
-// `clickable` has already been specified.
-document.querySelector('#actions .start').onclick = function() {
-  myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-};
-document.querySelector('#actions .cancel').onclick = function() {
-  myDropzone.removeAllFiles(true);
-};
