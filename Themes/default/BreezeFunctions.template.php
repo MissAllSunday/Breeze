f5ee977ef4ddc99e2623fe432624c0903cdcb6ab
Suki <suki@missallsunday.com>
2016-01-11 08:28:15 -0600

    Correct var is $status['profile_id']
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/BreezeFunctions.template.php
+++ Themes/default/BreezeFunctions.template.php
@@ -123,7 +123,7 @@ function breeze_status($data, $returnVar = false)
 										<textarea name="message" id="commentContent_'. $status['id'] .'" rel="atwhoMention"></textarea>
 										<input type="hidden" value="'. $status['poster_id'] .'" name="statusPoster" id="commentStatusPoster_'. $status['id'] .'" />
 										<input type="hidden" value="'. $status['id'] .'" name="statusID" id="commentStatus_'. $status['id'] .'" />
-										<input type="hidden" value="'. $status['profile_id'] .'" name="owner" id="commentOwner_'. $status['id'] .'" /><br />
+										<input type="hidden" value="'. $status['profile_id'] .'" name="owner" id="commentOwner_'. $status['profile_id'] .'" /><br />
 										<input type="hidden" id="'. $context['session_var'] .'" name="'. $context['session_var'] .'" value="'. $context['session_id'] .'" />
 										<input type="submit" value="'. $txt['post'] .'" class="button_submit clear" name="commentSubmit" id="commentSubmit_'. $status['id'] .'" />
 									</form>';
