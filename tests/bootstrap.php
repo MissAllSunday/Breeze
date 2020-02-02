d4f32e3e53b1e3ae88a03626cd45bfb5be4238d4
Jessica González <suki@missallsunday.com>
2020-02-01 20:39:29 -0600

    fix: add bootstrap.php file
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+define('ROOT', __DIR__);
+define('SMF', true);
+
+// mock globals used by SMF
+global $sourcedir, $scripturl, $modSettings;
+global $boarddir, $boardurl, $context, $txt, $smcFunc;
+
+// Function DB
+$smcFunc['htmltrim'] = function($value)
+{
+	return $value;
+};
+
+$smcFunc['htmlspecialchars'] = function($value)
+{
+	return $value;
+};
+
+// Mock functions
+function loadLanguage($template_name): void{}
+function log_error($string): void{}
+function add_integration_function(): void{}
+function remove_integration_function(): void{}
+function smf_json_decode($s, $array = true)
+{
+	return json_decode($s, $array);
+}
+
+$sourcedir = $scripturl = $boarddir = $boardurl = ROOT;
+
+// Mock some SMF arrays.
+$context = [
+    'session_var' => 'foo',
+    'session_id' => 'baz',
+];
+$modSettings = [
+    'CompressedOutput' => false,
+    'Breeze_someSetting' => 666,
+    'Breeze_master' => true,
+    'Breeze_time_machine' => false
+];
+
+$txt = [
+    'time_year' => 'years',
+    'time_month' => 'months',
+    'time_day' => 'days',
+    'time_hour' => 'hours',
+    'time_minute' => 'minutes',
+    'time_second' => 'seconds',
+    'time_ago' => 'ago',
+];
+
+// Composer-Autoloader
+require_once $_SERVER['DOCUMENT_ROOT'] . "./vendor/autoload.php";
