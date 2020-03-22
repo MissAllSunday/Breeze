caa4db328ca02371ce1a4c0a255a51441bc9b51f
Jessica González <suki@missallsunday.com>
2020-03-22 10:52:36 -0600

    fix: remove debugging stuff
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/Breeze/Breeze.php
+++ Sources/Breeze/Breeze.php
@@ -172,7 +172,7 @@ class Breeze
 		if (!empty($menu_buttons['profile']['sub_buttons']['summary']))
 			$menu_buttons['profile']['sub_buttons']['summary'] = [
 				'title' => $this->getText('summary'),
-				'href' => $scriptUrl . '?action=profile;area=static',
+				'href' => $scriptUrl . '?action=profile;area='. UserService::LEGACY_AREA,
 				'show' => true,
 			];
 
@@ -227,12 +227,12 @@ class Breeze
 	}
 
 	public function profilePopUpWrapper(&$profile_items): void
-	{return;
+	{
 		$this->container->get(UserService::class)->hookProfilePopUp($profile_items);
 	}
 
 	public function alertsPrefWrapper( array &$alertTypes, &$groupOptions): void
-	{return;
+	{
 		$this->container->get(UserService::class)->hookAlertsPref($alertTypes);
 	}
 
@@ -262,7 +262,7 @@ class Breeze
 	}
 
 	public function displayMoodWrapper( array &$data, int $userId, $displayCustomFields): void
-	{return;
+	{
 		/** @var MoodService */
 		$moodService = $this->container->get(MoodService::class);
 
@@ -271,8 +271,7 @@ class Breeze
 
 	public function displayMoodProfileWrapper($memID, $area): void
 	{
-		// Don't do anything if the mod is off
-		//if (!$this->enable('master'))
+		if (!$this->enable('master'))
 			return;
 
 		// Let BreezeMood handle this...
@@ -280,7 +279,7 @@ class Breeze
 	}
 
 	public function adminMenuWrapper(array &$adminMenu): void
-	{return;
+	{
 		/** @var AdminController */
 		$adminController = $this->container->get(AdminController::class);
 
