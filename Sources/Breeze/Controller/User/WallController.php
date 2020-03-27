7ce15d59cba0055c5ae05638bdda3ca3e73191ed
Jessica González <suki@missallsunday.com>
2020-03-27 16:50:04 -0600

    feat: WallService::setSubActionContent()
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/Breeze/Controller/User/WallController.php
+++ Sources/Breeze/Controller/User/WallController.php
@@ -12,10 +12,20 @@ class WallController extends BaseController implements ControllerInterface
 {
 	public const SUB_ACTIONS = [
 		'main',
+		'status',
+		'comment',
+		'userCard'
 	];
 
 	public function dispatch(): void
 	{
+		// Master setting is off, back off!
+		if (!$this->service->enable('master'))
+			fatal_lang_error('Breeze_error_no_valid_action', false);
+
+		// Guest aren't allowed, sorry.
+		is_not_guest($this->service->getText('error_no_access'));
+
 		$this->subActionCall();
 	}
 
--- Sources/Breeze/Service/WallService.php
+++ Sources/Breeze/Service/WallService.php
@@ -10,5 +10,34 @@ use Breeze\Service\ServiceInterface;
 
 class WallService extends BaseService implements ServiceInterface
 {
+	public const ACTION = 'breeze';
 
+	public function setSubActionContent(
+		string $actionName,
+		array $templateParams = [],
+		string $smfTemplate = ''
+	): void
+	{
+		if (empty($actionName))
+			return;
+
+		$context = $this->global('context');
+		$scriptUrl = $this->global('scripturl');
+
+		if (!isset($context[Breeze::NAME]))
+			$context[Breeze::NAME] = [];
+
+		if (!empty($templateParams))
+			$context = array_merge($context, $templateParams);
+
+		$context['page_title'] = $this->getText('general_wall');
+		$context['sub_template'] = !empty($smfTemplate) ?
+			$smfTemplate : (self::ACTION . '_' . $actionName);
+		$context['linktree'][] = [
+			'url' => $scriptUrl . '?action='. self::ACTION,
+			'name' => $context['page_title'],
+		];
+
+		$this->setGlobal('context', $context);
+	}
 }
