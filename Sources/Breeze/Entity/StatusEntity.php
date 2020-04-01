be92d9693740d571623598a8051eeb8d24beaf36
Jessica González <suki@missallsunday.com>
2020-03-31 21:39:15 -0600

    feat: UserSettingsEntity class
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/Breeze/Entity/StatusEntity.php
+++ Sources/Breeze/Entity/StatusEntity.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Breeze\Entity;
 
-class StatusEntity extends BaseEntity
+class StatusEntity extends BaseEntity implements EntityInterface
 {
 	const TABLE = 'breeze_status';
 	const COLUMN_ID = 'id';
---
+++ Sources/Breeze/Entity/UserSettingsEntity.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Breeze\Entity;
+
+class UserSettingsEntity extends BaseEntity implements EntityInterface
+{
+	public const WALL =  'wall';
+	public const GENERAL_WALL = 'generalWall';
+	public const PAGINATION_NUM = 'paginationNumber';
+	public const ALERT_NUM = 'alertNumber';
+	public const AUTO_LOADING = 'autoLoading';
+	public const ACTIVITY_LOG = 'activityLog';
+	public const KICK_IGNORED = 'kickIgnored';
+	public const BLOCK_LIST = 'blockList';
+	public const BUDDIES = 'buddies';
+	public const ABOUT_ME = 'aboutMe';
+
+	public static function getColumns(): array
+	{
+		return [
+			self::WALL,
+			self::GENERAL_WALL,
+			self::PAGINATION_NUM,
+			self::ALERT_NUM,
+			self::AUTO_LOADING,
+			self::ACTIVITY_LOG,
+			self::KICK_IGNORED,
+			self::BLOCK_LIST,
+			self::BUDDIES,
+			self::ABOUT_ME,
+		];
+	}
+}
