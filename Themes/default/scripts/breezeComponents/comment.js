49931e5fe5fc6c50cfc3d75dd1868c1bccd633cd
Jessica González <suki@missallsunday.com>
2020-04-25 16:16:45 -0500

    fix: use backticks for templates
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/scripts/breezeComponents/comment.js
+++ Themes/default/scripts/breezeComponents/comment.js
@@ -1,13 +1,19 @@
 Vue.component('comment', {
     props: ['comment', 'comment_poster_data'],
-    template: "<div>" +
-        "<div class='floatleft comment_user_info'>" +
-        "<div class='breeze_avatar avatar_comment' v-bind:style='avatarImage(comment_poster_data.avatar.href)'></div>" +
-        "<h4 v-html='comment_poster_data.link_color'></h4>" +
-        "</div>" +
-        "<div class='comment_date_info floatright smalltext'>{{comment.comments_time | formatDate}}</div>" +
-        "<div class='clear comment_content'><hr>{{comment.comments_body}}</div>" +
-        "</div>",
+    template: `
+    <div>
+        <div class='floatleft comment_user_info'>
+            <div class='breeze_avatar avatar_comment' v-bind:style='avatarImage(comment_poster_data.avatar.href)'></div>
+            <h4 v-html='comment_poster_data.link_color'></h4>
+        </div>
+        <div class='comment_date_info floatright smalltext'>
+            {{comment.comments_time | formatDate}}
+        </div>
+        <div class='clear comment_content'>
+            <hr>
+            {{comment.comments_body}}
+        </div>
+    </div>`,
     filters: {
         formatDate: function(unixTime) {
             return moment.unix(unixTime).format('LLLL')
--- Themes/default/scripts/breezeComponents/status.js
+++ Themes/default/scripts/breezeComponents/status.js
@@ -1,21 +1,26 @@
 Vue.component('status', {
     props: ['status_item', 'poster_data', 'comments'],
-    template: "<div>" +
-        "<div class='breeze_avatar floatleft' v-bind:style='avatarImage(poster_data.avatar.href)'></div>" +
-        "<div class='windowbg'>" +
-        "<h4 class='floatleft' v-html='poster_data.link_color'></h4>" +
-        "<div class='floatright smalltext'>{{status_item.status_time | formatDate}}</div><br>" +
-        "<div class='content'><hr>{{status_item.status_body}}</div>" +
-        "<comment " +
-            "v-for='comment in comments' " +
-            "v-bind:comment='comment' " +
-            "v-bind:comment_poster_data='getUserData(comment.comments_poster_id)' " +
-            "v-bind:key='comment.comments_id' " +
-            "class='windowbg' " +
-        ">" +
-        "</comment>" +
-        "</div>" +
-        "</div>",
+    template: `<div>
+    <div class='breeze_avatar floatleft' v-bind:style='avatarImage(poster_data.avatar.href)'></div>
+        <div class='windowbg'>
+            <h4 class='floatleft' v-html='poster_data.link_color'></h4>
+            <div class='floatright smalltext'>
+                {{status_item.status_time | formatDate}}
+            </div>
+            <br>
+            <div class='content'>
+                <hr>
+                {{status_item.status_body}}
+            </div>
+            <comment 
+                v-for='comment in comments' 
+                v-bind:comment='comment' 
+                v-bind:comment_poster_data='getUserData(comment.comments_poster_id)' 
+                v-bind:key='comment.comments_id' 
+                class='windowbg'>
+            </comment>
+        </div>
+    </div>`,
     filters: {
         formatDate: function(unixTime) {
             return moment.unix(unixTime).format('LLLL')
