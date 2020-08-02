eb31ea5c4684639715f94349cf9d6940b0b0bd1b
Jessica González <suki@missallsunday.com>
2020-08-02 09:57:05 -0500

    feat: status component postStatus()
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/scripts/breezeComponents/editor.js
+++ Themes/default/scripts/breezeComponents/editor.js
@@ -8,10 +8,9 @@ Vue.component('editor', {
             notice: null,
             place_holder: 'say something!',
             post_status: {
-                status_owner_id: wall_owner_id,
-                status_poster_id: smf_member_id,
-                status_body: ''
-                ,
+                status_owner_id: parseInt(wall_owner_id),
+                status_poster_id: parseInt(smf_member_id),
+                status_body: '',
             },
         }
     },
@@ -49,11 +48,56 @@ Vue.component('editor', {
     },
     methods: {
         postStatus: function (){
+            this.clearNotice()
 
+            this.post_status.status_body = this.editor.getContents(true)
+
+            if (!this.isValidStatus())
+                return false;
+
+            axios.post(smf_scripturl + '?action=breezeStatus;sa=postStatus;'+ smf_session_var +'='+ smf_session_id,
+                this.post_status).then(response => {
+
+                this.setNotice(response.data.message, response.data.type);
+
+                if (response.data.content){
+                    this.$root.setUserData(response.data.content.users)
+                }
+
+                this.clearPostComment();
+            });
         },
         preview: function (){
             this.previewed = this.previewed === null ? this.editor.getContents(true) : null
             this.preview_name = this.previewed === null ? 'Preview' : 'Clear'
-        }
+        },
+        isValidStatus: function () {
+            if (this.post_status.status_body === '' ||
+                this.post_status.status_body === '<p><br></p>' ||
+                typeof(this.post_status.status_body) !== 'string' )
+            {
+                this.setNotice('el body esta vacio');
+
+                return false;
+            }
+
+            return true;
+        },
+        setNotice: function(message, type){
+            type = type || 'error';
+            let $this = this;
+            this.notice = true;
+
+            Vue.$toast.open({
+                message: message,
+                type: type,
+                onClose: function () {
+                    $this.notice = null;
+                }
+            });
+        },
+        clearNotice: function(){
+            this.notice = null;
+        },
     }
 })
