7f29a319777b803fc8d6df708f229a016468b876
Jessica González <suki@missallsunday.com>
2020-08-02 09:11:28 -0500

    feat: Implement previewing a status
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/scripts/breezeComponents/editor.js
+++ Themes/default/scripts/breezeComponents/editor.js
@@ -2,6 +2,8 @@ Vue.component('editor', {
     props: ['editor_options'],
     data: function() {
         return {
+            previewed: null,
+            preview_name: 'Preview',
             editor: null,
             notice: null,
             place_holder: 'say something!',
@@ -13,16 +15,28 @@ Vue.component('editor', {
             },
         }
     },
-    template: `<div>
-        <textarea id="Breeze_editor">Hi</textarea>
+    template: `
+    <div>
+        <div v-if="previewed !== null" class="cat_bar">
+            <h3 class="catbg">
+                Preview
+            </h3>
+        </div>
+        <div v-if="previewed !== null" class="sun-editor-editable windowbg" v-html="previewed">
+        </div>
+        <textarea id="Breeze_editor"></textarea>
+        <div v-if="notice === null" class="post_button_container floatright">
+                <input type="button" @click="preview()" class="button" :value="preview_name">
+                <input type="submit" @click="postStatus()" class="button">
+        </div>
     </div>`,
     mounted: function() {
         this.editor = SUNEDITOR.create(document.getElementById('Breeze_editor'),{
             width : 'auto',
-            maxWidth : '700px',
+            maxWidth : '1200px',
             height : 'auto',
             minHeight : '100px',
-            maxHeight: '250px',
+            maxHeight: '350px',
             fontSize : [
                 8, 10, 14, 18, 24
             ],
@@ -34,6 +48,12 @@ Vue.component('editor', {
         });
     },
     methods: {
+        postStatus: function (){
 
+        },
+        preview: function (){
+            this.previewed = this.previewed === null ? this.editor.getContents(true) : null
+            this.preview_name = this.previewed === null ? 'Preview' : 'Clear'
+        }
     }
 })
