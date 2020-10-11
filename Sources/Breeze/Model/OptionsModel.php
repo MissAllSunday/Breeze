61718361543643683539ca5caf631fca70519810
Jessica González <suki@missallsunday.com>
2020-10-11 09:22:07 -0500

    feat: OptionsModel.php
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ Sources/Breeze/Model/OptionsModel.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Breeze\Model;
+
+
+class OptionsModel extends BaseModel implements OptionsModelInterface
+{
+	public const SAVED_AS_JSON = ['moodHistory'];
+
+	public function insert(array $data, int $id = 0): int
+	{
+		if (empty($data) || empty($userId))
+			return 0;
+
+		$inserts = [];
+
+		foreach ($data as $settingVariable => $settingValue)
+		{
+			// Does the value needs to be encoded?
+			if (in_array($var, $this->_needJSON))
+				$val = !empty($val) ? json_encode($val) : '';
+
+			$inserts[] = [$userID, $var, $val];
+		}
+
+		if (!empty($inserts))
+			$smcFunc['db_insert'](
+				'replace',
+				'{db_prefix}' . ($this->_tables['options']['table']),
+				['member_id' => 'int', 'variable' => 'string-255', 'value' => 'string-65534'],
+				$inserts,
+				['member_id']
+			);
+
+		// Force getting the new settings.
+		return $this->getUserSettings($userID);
+	}
+
+	public function update(array $data, int $id = 0): array
+	{
+		return [];
+	}
+
+	public function getTableName(): string
+	{
+		return '';
+	}
+
+	public function getColumnId(): string
+	{
+		return '';
+	}
+
+	public function getColumns(): array
+	{
+		return [];
+	}
+
+}
---
+++ Sources/Breeze/Model/OptionsModelInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Breeze\Model;
+
+
+interface OptionsModelInterface
+{
+
+}
