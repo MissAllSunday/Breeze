4f08420dc08e97e0bd96fe7cf65fed57911dab6a
Jessica González <suki@missallsunday.com>
2020-12-26 09:31:21 -0600

    feat: API MoodController.php
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ Sources/Breeze/Controller/API/MoodController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Breeze\Controller\API;
+
+use Breeze\Service\MoodServiceInterface;
+use Breeze\Service\UserServiceInterface;
+use Breeze\Util\Validate\ValidateGatewayInterface;
+use Breeze\Util\Validate\Validations\ValidateData;
+use Breeze\Util\Validate\Validations\ValidateDataInterface;
+
+class MoodController extends ApiBaseController implements ApiBaseInterface
+{
+	public const ACTION_POST = 'postComment';
+	public const ACTION_DELETE = 'deleteComment';
+	public const ACTION_PATCH = 'deleteComment';
+	public const SUB_ACTIONS = [
+		self::ACTION_POST,
+		self::ACTION_DELETE,
+		self::ACTION_PATCH,
+	];
+
+	private UserServiceInterface $userService;
+
+	private MoodServiceInterface $moodService;
+
+	public function __construct(
+		UserServiceInterface $userService,
+		MoodServiceInterface $moodService,
+		ValidateGatewayInterface $gateway
+	) {
+		parent::__construct($gateway);
+		$this->userService = $userService;
+		$this->moodService = $moodService;
+		$this->gateway = $gateway;
+	}
+
+	public function getSubActions(): array
+	{
+		return self::SUB_ACTIONS;
+	}
+
+	public function getMainAction(): string
+	{
+		return self::ACTION_POST;
+	}
+
+	public function setValidator(): ValidateDataInterface
+	{
+		$validatorName = ValidateData::getNameSpace() . ucfirst($this->subAction);
+
+		return new $validatorName(
+			$this->userService,
+			$this->moodService
+		);
+	}
+
+	public function render(string $subTemplate, array $params): void
+	{
+	}
+}
