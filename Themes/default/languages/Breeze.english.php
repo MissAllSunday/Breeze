61d1e2460d46f4c8ee4d0487421b03e07209a905
Jessica González <suki@missallsunday.com>
2020-12-29 10:57:09 -0600

    fix: no need
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/languages/Breeze.english.php
+++ Themes/default/languages/Breeze.english.php
@@ -28,20 +28,24 @@ $txt['Breeze_user_settings_main_desc'] = 'Configure your wall and other general 
 $txt['Breeze_user_buddysettings_name'] = 'Buddy Requests';
 $txt['Breeze_user_single_status'] = 'Single Status';
 $txt['Breeze_user_settings_paginationNumber'] = 'How many status will be displayed per page';
-$txt['Breeze_user_settings_paginationNumber_desc'] = 'Enter the number of status you want to display per page on your wall. By default is 5';
+$txt['Breeze_user_settings_paginationNumber_desc'] = 'Enter the number of status you want
+ to display per page on your wall. By default is 5';
 $txt['Breeze_user_settings_generalWall'] = 'Enable the general wall';
-$txt['Breeze_user_settings_generalWall_desc'] = 'The general wall is a unique page where you can see the status and updates from your buddies. This setting allows you to enable/disable it.';
+$txt['Breeze_user_settings_generalWall_desc'] = 'The general wall is a unique page where you can see the status
+ and updates from your buddies. This setting allows you to enable/disable it.';
 $txt['Breeze_user_settings_submit'] = 'Submit';
 $txt['Breeze_user_settings_cancel'] = 'Cancel';
 $txt['Breeze_user_settings_checkAll'] = 'select all options';
 $txt['Breeze_user_settings_wall'] = 'Enable my wall';
-$txt['Breeze_user_settings_wall_desc'] = 'If you want to use your wall, you need to check this option, otherwise your profile will show the default page.';
+$txt['Breeze_user_settings_wall_desc'] = 'If you want to use your wall, you need to check this option,
+otherwise your profile will show the default page.';
 $txt['Breeze_user_settings_aboutMe'] = 'Enable the About me tab';
 $txt['Breeze_user_settings_aboutMe_desc'] = 'Leave empty to disable it. You can use BBC.';
 $txt['Breeze_user_settings_buddies'] = 'Enable the "Buddies" block.';
 $txt['Breeze_user_settings_buddies_desc'] = 'This will show a block div showing all your buddies and their info.';
 $txt['Breeze_user_settings_kickIgnored'] = 'Do not show my wall to users in my ignore list';
-$txt['Breeze_user_settings_kickIgnored_desc'] = 'If this is enable, users on your ignore list will not be able to see your wall or to post on it.';
+$txt['Breeze_user_settings_kickIgnored_desc'] = 'If this is enable, users on your ignore list will not be able to
+ see your wall or to post on it.';
 $txt['Breeze_user_permissions_name'] = 'Validate';
 $txt['Breeze_user_modules_visitors_none'] = 'There are no recent visitors';
 $txt['Breeze_user_modules_buddies_none'] = 'This user doesn\'t have any buddies';
@@ -102,10 +106,6 @@ $txt['Breeze_info_notiMulti_delete_after'] = 'You have successfully deleted all 
 $txt['Breeze_info_notiMulti_markasread_after'] = 'You have successfully marked as read all notifications';
 $txt['Breeze_info_notiMulti_unmarkasread_after'] = 'You have successfully marked as unread all notifications';
 
-
-// TODO: move $scripurl to back
-$txt['Breeze_cannot_see_general_wall'] = 'You need to enable your general wall from your <a href="' . $scripturl . '?action=profile;area=breezesettings">wall settings page</a>.';
-
 // Loading text.
 $txt['Breeze_profile_of_username'] = 'Profile of {name}';
 $txt['Breeze_info_loading_end'] = 'There are no more status to display';
@@ -139,18 +139,22 @@ $txt['Breeze_buddy_title'] = 'Buddy request';
 $txt['Breeze_buddy_confirm'] = 'The invitation has been sent, {receiver} will soon receive an alert.';
 $txt['Breeze_buddy_sender_message_title'] = '{sender} sent the following message';
 $txt['Breeze_buddy_chose_title'] = '{sender} wants to be your buddy!';
-$txt['Breeze_buddy_chose'] = 'Please <a href="{href_confirm}"><i class="fa fa-user-plus fa-2x"></i> confirm</a> or <a href="{href_decline}"><i class="fa fa-user-times fa-2x"></i> decline</a> the invitation.';
+$txt['Breeze_buddy_chose'] = 'Please <a href="{href_confirm}"><i class="fa fa-user-plus fa-2x"></i> confirm</a> or
+ <a href="{href_decline}"><i class="fa fa-user-times fa-2x"></i> decline</a> the invitation.';
 $txt['Breeze_buddy_already_buddy'] = 'You and {receiver} are already buddies.';
 $txt['Breeze_buddy_blocked'] = 'You cannot send an invitation to this user.';
 $txt['Breeze_buddy_already_blocked'] = 'You already added this user to your block list.';
-$txt['Breeze_buddy_decline'] = 'You have declined the invitation.<br>Do you want to block this person from sending you more invites? this WILL NOT put this person on your ignore list, it will merely prevent this user from sending you a buddy invite<br><a href="{href}">yes, block this user</a>';
+$txt['Breeze_buddy_decline'] = 'You have declined the invitation.<br>
+Do you want to block this person from sending you more invites? this WILL NOT put this person on your ignore list,
+ it will merely prevent this user from sending you a buddy invite<br><a href="{href}">yes, block this user</a>';
 $txt['Breeze_buddy_blocked_done'] = 'You have successfully blocked this user';
 $txt['Breeze_buddy_delete_done'] = 'You and {receiver} are no longer buddies.';
 $txt['Breeze_buddy_confirmed_done'] = 'You have successfully confirmed the buddy request.';
 $txt['Breeze_buddy_already_sent'] = 'You already sent an invitation, please wait for {receiver} to respond';
 $txt['Breeze_buddy_error'] = 'There was an error, please try again.';
 $txt['Breeze_buddy_message'] = 'Buddy message';
-$txt['Breeze_buddy_message_desc'] = 'You can send {receiver} a message along with your buddy invite. <br> no HTML or BBC is allowed. Leave the field empty if you do not want to send a message.';
+$txt['Breeze_buddy_message_desc'] = 'You can send {receiver} a message along with your buddy invite. <br>
+ no HTML or BBC is allowed. Leave the field empty if you do not want to send a message.';
 
 // Errors
 $txt['cannot_breeze_postStatus'] = $txt['Breeze_error_postStatus'];
