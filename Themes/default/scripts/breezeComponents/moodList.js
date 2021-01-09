aba36d7a76ef374649c80ff5f04c35a31001abc5
Jessica González <suki@missallsunday.com>
2021-01-09 17:46:32 -0600

    fix: change mood column name
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/scripts/breezeComponents/moodList.js
+++ Themes/default/scripts/breezeComponents/moodList.js
@@ -14,7 +14,7 @@ new Vue({
 		moodsURL: {
 			type: String,
 			required: false,
-			default: smf_scripturl + '?action=breezeMood;sa=getActiveMoods;'+ smf_session_var +'='+ smf_session_id
+			default: smf_scripturl + '?action=breezeMood;sa=getAllMoods;'+ smf_session_var +'='+ smf_session_id
 		},
 	},
 	created: function () {
@@ -28,6 +28,8 @@ new Vue({
 					let moodCount = Object.keys(response.data).length
 					for (let i = 1; i <= moodCount; i++) {
 						response.data[i].emoji = this.decode(response.data[i].emoji)
+						response.data[i].active = Boolean(Number(response.data[i].active))
+						response.data[i].moods_id = Number(response.data[i].moods_id)
 					}
 
 					this.localMoods = response.data
