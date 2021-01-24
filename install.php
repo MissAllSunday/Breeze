12a3c967d8a7a6c2a878544cacca7072bc499570
Jessica González <suki@missallsunday.com>
2021-01-24 08:26:34 -0600

    feat: add extra columns to their correct tables
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- install.php
+++ install.php
@@ -6,11 +6,11 @@ declare(strict_types=1);
  * @license http://www.mozilla.org/MPL/ MPL 2.0
  */
 
-if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF')) {
 	require_once(dirname(__FILE__) . '/SSI.php');
-
-elseif (!defined('SMF'))
+} elseif (!defined('SMF')) {
 	exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+}
 
 global $smcFunc, $context;
 
@@ -18,8 +18,7 @@ BreezeCheck();
 
 db_extend('packages');
 
-if (empty($context['uninstalling']))
-{
+if (empty($context['uninstalling'])) {
 	// Member options
 	$tables[] = [
 		'table_name' => '{db_prefix}breeze_options',
@@ -46,7 +45,7 @@ if (empty($context['uninstalling']))
 		'indexes' => [
 			[
 				'type' => 'primary',
-				'columns' => ['member_id', 'variable']
+				'columns' => ['member_id','variable']
 			],
 		],
 		'if_exists' => 'ignore',
@@ -78,10 +77,10 @@ if (empty($context['uninstalling']))
 				'null' => false
 			],
 			[
-				'name' => 'createdAt',
-				'type' => 'varchar',
-				'size' => 255,
-				'default' => '',
+				'name' => 'likes',
+				'type' => 'int',
+				'size' => 5,
+				'null' => false
 			],
 			[
 				'name' => 'body',
@@ -89,11 +88,17 @@ if (empty($context['uninstalling']))
 				'size' => '',
 				'default' => null,
 			],
+			[
+				'name' => 'createdAt',
+				'type' => 'varchar',
+				'size' => 255,
+				'default' => '',
+			],
 		],
 		'indexes' => [
 			[
 				'type' => 'primary',
-				'columns' => ['id', 'statusId']
+				'columns' => ['id','statusId']
 			],
 		],
 		'if_exists' => 'ignore',
@@ -125,10 +130,10 @@ if (empty($context['uninstalling']))
 				'null' => false
 			],
 			[
-				'name' => 'createdAt',
-				'type' => 'varchar',
-				'size' => 255,
-				'default' => '',
+				'name' => 'likes',
+				'type' => 'int',
+				'size' => 5,
+				'null' => false
 			],
 			[
 				'name' => 'body',
@@ -136,11 +141,17 @@ if (empty($context['uninstalling']))
 				'size' => '',
 				'default' => null,
 			],
+			[
+				'name' => 'createdAt',
+				'type' => 'varchar',
+				'size' => 255,
+				'default' => '',
+			],
 		],
 		'indexes' => [
 			[
 				'type' => 'primary',
-				'columns' => ['id', 'wallId']
+				'columns' => ['id','wallId']
 			],
 		],
 		'if_exists' => 'ignore',
@@ -202,46 +213,27 @@ if (empty($context['uninstalling']))
 	];
 
 	// Installing
-	foreach ($tables as $table)
-		$smcFunc['db_create_table']($table['table_name'],
+	foreach ($tables as $table) {
+		$smcFunc['db_create_table'](
+			$table['table_name'],
 			$table['columns'],
 			$table['indexes'],
 			$table['parameters'],
 			$table['if_exists'],
 			$table['error']
 		);
-
-	// Add the extra columns
-	$smcFunc['db_add_column'](
-		'{db_prefix}breeze_status',
-		[
-			'name' => 'likes',
-			'type' => 'int',
-			'size' => 5,
-			'null' => false
-		],
-		[],
-		'update',
-		null
-	);
-	$smcFunc['db_add_column'](
-		'{db_prefix}breeze_comments',
-		[
-			'name' => 'likes',
-			'type' => 'int',
-			'size' => 5,
-			'null' => false
-		],
-		[],
-		'update',
-		null
-	);
+	}
 
 	// Lastly, insert the default moods and oh boy there are a lot!!!
-	$emojis = explode(',', trim('😀,😁, 😂, 🤣, 😃, 😄, 😅, 😆, 😉, 😊, 😋, 😎, 😍, 😘, 🥰, 😗, 😙, 😚, ☺️, 🙂, 🤗, 🤩, 🤔, 🤨, 😐, 😑, 😶, 🙄, 😏, 😣, 😥, 😮, 🤐, 😯, 😪, 😫, 😴, 😌, 😛, 😜, 😝, 🤤, 😒, 😓, 😔, 😕, 🙃, 🤑, 😲, ☹️, 🙁, 😖, 😞, 😟, 😤, 😢, 😭, 😦, 😧, 😨, 😩, 🤯, 😬, 😰, 😱, 🥵, 🥶, 😳, 🤪, 😵, 😡, 😠, 🤬, 😷, 🤒, 🤕, 🤢, 🤮, 🤧, 😇, 🤠, 🤡, 🥳, 🥴, 🥺, 🤥, 🤫, 🤭, 🧐, 🤓, 😈, 👿, 👹, 👺,💩'));
+	$emojis = explode(
+		',',
+		trim(
+			'😀,😁,😂,🤣,😃,😄,😅,😆,😉,😊,😋,😎,😍,😘,🥰,😗,😙,😚,☺️,🙂,🤗,🤩,🤔,🤨,😐,😑,😶,🙄,😏,😣,😥,😮,🤐,😯,😪,😫,😴,😌,😛,😜,😝,🤤,😒,😓,😔,😕,🙃,🤑,😲,☹️,🙁,😖,😞,😟,😤,😢,😭,😦,😧,😨,😩,🤯,😬,😰,😱,🥵,🥶,😳,🤪,😵,😡,😠,🤬,😷,🤒,🤕,🤢,🤮,🤧,😇,🤠,🤡,🥳,🥴,🥺,🤥,🤫,🤭,🧐,🤓,😈,👿,👹,👺,💩'
+		)
+	);
 
 
-	foreach ($emojis as $emoji)
+	foreach ($emojis as $emoji) {
 		$smcFunc['db_insert']('insert', '{db_prefix}breeze_moods', [
 			'emoji' => 'string',
 			'description' => 'string',
@@ -252,9 +244,12 @@ if (empty($context['uninstalling']))
 			1
 		], ['id']);
 	}
+}
 
 function BreezeCheck(): void
 {
-	if (version_compare(PHP_VERSION, '7.4.0', '<'))
-		fatal_error('This mod needs PHP 7.4 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
+	if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+		fatal_error('This mod needs PHP 7.4 or greater.
+		 You will not be able to install/use this mod,contact your host and ask for a php upgrade.');
+	}
 }
