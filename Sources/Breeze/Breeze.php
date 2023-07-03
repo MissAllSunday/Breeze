fe5bc18d778f77b76f645be4a32a4a76434647ad
Michel Mendiola <suki@missallsunday.com>
2023-07-03 12:54:38 -0600

    feat: update react hash
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

--- Sources/Breeze/Breeze.php
+++ Sources/Breeze/Breeze.php
@@ -37,7 +37,7 @@ class Breeze
 	public const SUPPORT_URL = 'https://missallsunday.com';
 	public const REACT_DOM_VERSION = '18.2.0';
 	public const REACT_VERSION = '18.2.0';
-	public const REACT_HASH = 'a6cc02b9';
+	public const REACT_HASH = '20142cbb';
 	public const ACTIONS = [
 		'breezeStatus',
 		'breezeComment',
