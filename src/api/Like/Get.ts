1e4a80238f073ef41743fd096feb82163ba5bce5
Michel Mendiola <suki@missallsunday.com>
2024-09-24 08:50:04 -0600

    feat: split Like calls
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

---
+++ src/api/Like/Get.ts
@@ -0,0 +1,25 @@
+import { LikeInfoState, LikeType } from 'breezeTypesLikes';
+
+import smfTextVars from '../../DataSource/Txt';
+import { showError } from '../../utils/tooltip';
+import { baseUrl, resolveGet } from '../Api';
+
+export const getLikeInfo = async (like: LikeType):Promise<any> => {
+  try {
+    const params = {
+      content_type: like.type,
+      content_id: like.contentId,
+    };
+    const response =  await fetch(baseUrl('breezeLike', 'info',
+      [ params ]), {
+      method: 'GET',
+      headers: {
+        'X-SMF-AJAX': '1',
+      },
+    });
+
+    return await resolveGet(response);
+  } catch (error:unknown) {
+    showError(smfTextVars.error.generic);
+  }
+};
---
+++ src/api/Like/Post.ts
@@ -0,0 +1,31 @@
+import { LikeType } from 'breezeTypesLikes';
+
+import SmfVars from '../../DataSource/SMF';
+import smfTextVars from '../../DataSource/Txt';
+import { showError } from '../../utils/tooltip';
+import { baseConfig, baseUrl, resolvePost } from '../Api';
+
+export interface IPostLikeParams {
+  id_member: number;
+  content_type: string;
+  content_id: number;
+}
+
+export const postLike = async (likeData: LikeType): Promise<any> => {
+  try {
+    const params:IPostLikeParams = {
+      id_member: SmfVars.userId,
+      content_type: likeData.type,
+      content_id: likeData.contentId,
+    };
+
+    const likeResults = await fetch(baseUrl('breezeLike', 'like'), {
+      method: 'POST',
+      body: JSON.stringify(baseConfig(params)),
+    });
+
+    return await resolvePost(likeResults);
+  } catch (error:unknown) {
+    showError(smfTextVars.error.generic);
+  }
+};
