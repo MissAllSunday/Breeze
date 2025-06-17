6b2625a32e0f22de6419e822a6ef0ac985f78d07
Michel Mendiola <suki@missallsunday.com>
2025-06-17 14:09:19 -0600

    fix: userInfo
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

--- Sources/Breeze/Entity/CommentHandledEntity.php
+++ Sources/Breeze/Entity/CommentHandledEntity.php
@@ -50,7 +50,7 @@ class CommentHandledEntity extends CommentEntity implements HandledEntityInterfa
 			'body' => $this->getBody(),
 			'likes' => $this->getLikesInfo()->getCount(),
 			'likesInfo' => $this->getLikesInfo(),
-			'userData' => $this->getUsersInfo(),
+			'userInfo' => $this->getUsersInfo(),
 		];
 	}
 }
--- Sources/Breeze/Entity/StatusHandledEntity.php
+++ Sources/Breeze/Entity/StatusHandledEntity.php
@@ -71,7 +71,7 @@ class StatusHandledEntity extends StatusEntity implements HandledEntityInterface
 			'createdAt' => $this->getCreatedAt()->format(DateTimeInterface::ATOM),
 			'likesInfo' => $this->getLikesInfo(),
 			'comments' => $this->getComments(),
-			'userData' => $this->getUsersInfo()[$this->getUserId()] ?? [],
+			'userInfo' => $this->getUsersInfo()[$this->getUserId()] ?? [],
 			'isNew' => $this->isNew(),
 		];
 	}
