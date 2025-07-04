a2fd93492a45dd239b5ce29c5cdf37706637d6e5
Michel Mendiola <suki@missallsunday.com>
2025-07-04 09:46:02 -0600

    feat: remove not needed NotificationEntity
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

--- Sources/Breeze/Entity/MentionEntity.php
+++ Sources/Breeze/Entity/MentionEntity.php
@@ -5,22 +5,25 @@ declare(strict_types=1);
 
 namespace Breeze\Entity;
 
+use DateMalformedStringException;
+use DateTimeImmutable;
+
 class MentionEntity extends NormalizedEntity implements EntityInterface
 {
-	public const TABLE = 'mentions';
-	public const COLUMN_CONTENT_ID = 'content_id';
-	public const COLUMN_CONTENT_TYPE = 'content_type';
-	public const COLUMN_ID_MENTIONED = 'id_mentioned';
-	public const COLUMN_ID_MEMBER = 'id_member';
-	public const COLUMN_TIME = 'time';
+	public const string TABLE = 'mentions';
+	public const string COLUMN_CONTENT_ID = 'content_id';
+	public const string COLUMN_CONTENT_TYPE = 'content_type';
+	public const string COLUMN_ID_MENTIONED = 'id_mentioned';
+	public const string COLUMN_ID_MEMBER = 'id_member';
+	public const string COLUMN_TIME = 'time';
 
-	public const PROPERTY_CONTENT_ID = 'id';
-	public const PROPERTY_CONTENT_TYPE = 'type';
-	public const PROPERTY_ID_MENTIONED = 'idMentioned';
-	public const PROPERTY_ID_MEMBER = 'idMember';
-	public const PROPERTY_TIME = 'time';
+	public const string PROPERTY_CONTENT_ID = 'id';
+	public const string PROPERTY_CONTENT_TYPE = 'type';
+	public const string PROPERTY_ID_MENTIONED = 'idMentioned';
+	public const string PROPERTY_ID_MEMBER = 'idMember';
+	public const string PROPERTY_TIME = 'time';
 
-	public const KEY_MAP = [
+	public const array KEY_MAP = [
 		self::COLUMN_CONTENT_ID => self::PROPERTY_CONTENT_ID,
 		self::COLUMN_CONTENT_TYPE => self::PROPERTY_CONTENT_TYPE,
 		self::COLUMN_ID_MENTIONED => self::PROPERTY_ID_MENTIONED,
@@ -47,5 +50,19 @@ class MentionEntity extends NormalizedEntity implements EntityInterface
 	public function getColumnMap(): array
 	{
 		return self::KEY_MAP;
+	}
+
+	/**
+	 * @throws DateMalformedStringException
+	 */
+	public function castValue(string $columnName, mixed $value): string|int
+	{
+		return match ($columnName) {
+			self::COLUMN_CONTENT_ID,
+			self::COLUMN_ID_MENTIONED,
+			self::COLUMN_ID_MEMBER => (int) $value,
+			self::COLUMN_TIME => new DateTimeImmutable('@' . $value),
+			default => (string) $value,
+		};
 	}
 }
