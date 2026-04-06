729cae963ace61e5e52c378e1516102f541b534e
Michel Mendiola <suki@missallsunday.com>
2026-04-06 12:53:48 -0600

    fix: use SMFs poster info on mini profile modal
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

--- src/components/user/MiniProfile.tsx
+++ src/components/user/MiniProfile.tsx
@@ -21,74 +21,77 @@ const MiniProfile: React.FunctionComponent<MiniProfileProps> = (
 
 	const profileBody = (
 		<div className="mini_profile_body">
-			<div className="mini_profile_header">
-				<Avatar
-					href={userData.avatar.url}
-					userName={userData.username}
-					customClassName="mini_profile_avatar"
-				/>
-				<div className="mini_profile_identity">
-					<span
-						className="mini_profile_name"
-						dangerouslySetInnerHTML={{ __html: userData.link_color }}
-					/>
-					<span className="mini_profile_group">
-						{userData.group} {onlineIndicator}
-					</span>
-					<span
-						className="mini_profile_icons"
-						dangerouslySetInnerHTML={{ __html: userData.group_icons }}
-					/>
-				</div>
-			</div>
-
-			{userData.title && (
-				<div className="mini_profile_field">
-					<span className="mini_profile_label">Title</span>
-					<span className="mini_profile_value">{userData.title}</span>
-				</div>
-			)}
-
-			{userData.signature && (
-				<div className="mini_profile_field">
-					<span className="mini_profile_label">Signature</span>
-					<span
-						className="mini_profile_value"
-						dangerouslySetInnerHTML={{ __html: userData.signature }}
-					/>
-				</div>
-			)}
-
-			{userData.last_login_timestamp && (
-				<div className="mini_profile_field">
-					<span className="mini_profile_label">Last Active</span>
-					<span className="mini_profile_value">
-						{userData.last_login_timestamp}
-					</span>
-				</div>
-			)}
-
-			{userData.custom_fields?.length > 0 && (
-				<div className="mini_profile_field">
-					<span className="mini_profile_label">Custom Fields</span>
-					<ul className="mini_profile_custom_fields">
-						{userData.custom_fields.map((field: CustomFieldType) => (
-							<li key={field.col_name}>
-								<strong>{field.title}</strong>:{" "}
-								<span dangerouslySetInnerHTML={{ __html: field.value }} />
-							</li>
-						))}
+			<div className="post_wrapper">
+				<div className="poster">
+					<ul className="user_info">
+						<li className="avatar">
+							<Avatar
+								href={userData.avatar.url}
+								userName={userData.username}
+								customClassName="mini_profile_avatar"
+							/>
+						</li>
+						<li>
+							<span
+								className="mini_profile_name"
+								dangerouslySetInnerHTML={{ __html: userData.link_color }}
+							/>
+						</li>
+						<li className="postgroup">
+							{userData.group} {onlineIndicator}
+						</li>
+						<li
+							className="icons"
+							dangerouslySetInnerHTML={{ __html: userData.group_icons }}
+						/>
+						{userData.title && (
+							<li className="breeze_description">{userData.title}</li>
+						)}
 					</ul>
 				</div>
-			)}
+				<div className="postarea">
+					{userData.signature && (
+						<div className="mini_profile_field">
+							<span className="mini_profile_label">Signature</span>
+							<span
+								className="mini_profile_value"
+								dangerouslySetInnerHTML={{ __html: userData.signature }}
+							/>
+						</div>
+					)}
 
-			{userData.href && (
-				<div className="mini_profile_actions">
-					<a href={userData.href} className="button">
-						View Full Profile
-					</a>
+					{userData.last_login_timestamp && (
+						<div className="mini_profile_field">
+							<span className="mini_profile_label">Last Active</span>
+							<span className="mini_profile_value">
+								{userData.last_login_timestamp}
+							</span>
+						</div>
+					)}
+
+					{userData.custom_fields?.length > 0 && (
+						<div className="mini_profile_field">
+							<span className="mini_profile_label">Custom Fields</span>
+							<ul className="mini_profile_custom_fields">
+								{userData.custom_fields.map((field: CustomFieldType) => (
+									<li key={field.col_name}>
+										<strong>{field.title}</strong>:{" "}
+										<span dangerouslySetInnerHTML={{ __html: field.value }} />
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+
+					{userData.href && (
+						<div className="mini_profile_actions">
+							<a href={userData.href} className="button">
+								View Full Profile
+							</a>
+						</div>
+					)}
 				</div>
-			)}
+			</div>
 		</div>
 	);
 
