40fe99752c4709bfb2115801f96cdf0f92fd382d
Michel Mendiola <suki@missallsunday.com>
2026-04-20 13:11:00 -0600

    fix: another generic_menu in the wild!
    Signed-off-by: Michel Mendiola <suki@missallsunday.com>

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -6,10 +6,8 @@ interface ButtonsProps {
 
 export default function Buttons({ children }: ButtonsProps) {
   return (
-    <div id="Breeze_buttons" className="generic_menu">
       <ul className="dropmenu breezeButtons">
         {children}
       </ul>
-    </div>
   );
 }
