54b9dab15f98f030159d8725fcd7f7421ad2fd98
Michel Mendiola <suki@missallsunday.com>
2026-07-15 10:21:17 -0600

    chore: add a few more tests

--- tests/Repository/LikeRepositoryTest.php
+++ tests/Repository/LikeRepositoryTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Breeze\Repository;
 
 use Breeze\Database\ClientInterface;
+use Breeze\Entity\LikeEntity;
 use Breeze\Entity\LikeInfoEntity;
 use Breeze\Enums\LikesEnum;
 use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
@@ -34,18 +35,50 @@ class LikeRepositoryTest extends TestCase
 		$this->assertEquals('user_likes', $this->likeRepository->getTableName());
 	}
 
+	public function testGetColumnId(): void
+	{
+		$this->assertEquals('content_id', $this->likeRepository->getColumnId());
+	}
+
+	public function testGetColumnPosterId(): void
+	{
+		$this->assertEquals('id_member', $this->likeRepository->getColumnPosterId());
+	}
+
+	public function testGetColumns(): void
+	{
+		$expected = [
+			'id_member',
+			'content_type',
+			'content_id',
+			'like_time',
+		];
+		$this->assertEquals($expected, $this->likeRepository->getColumns());
+	}
+
 	#[DataProvider('getByContentProvider')]
 	public function testGetByContent(LikesEnum $type, array $contentIds, array $dbResult, int $expectedCount): void
 	{
 		$this->dbClient->expects($this->once())
 			->method('query')
 			->willReturn($dbResult);
-		$this->dbClient->expects($this->exactly(2))
-			->method('fetchAssoc')
-			->willReturnOnConsecutiveCalls(
-				reset($dbResult),
-				null
-			);
+
+		if (empty($dbResult)) {
+			// For empty results, fetchAssoc should be called once and return null
+			$this->dbClient->expects($this->once())
+				->method('fetchAssoc')
+				->with($dbResult)
+				->willReturn(null);
+		} else {
+			// For non-empty results, fetchAssoc should be called multiple times
+			$this->dbClient->expects($this->exactly(2))
+				->method('fetchAssoc')
+				->willReturnOnConsecutiveCalls(
+					reset($dbResult),
+					null
+				);
+		}
+
 		$this->dbClient->expects($this->once())
 			->method('freeResult');
 
@@ -66,6 +99,40 @@ class LikeRepositoryTest extends TestCase
 				[['content_id' => 1, 'content_type' => LikesEnum::Status->value, 'like_time' => 5, 'id_member' => 1]],
 				2,
 			],
+			'comment likes found' => [
+				LikesEnum::Comments,
+				[3, 4],
+				[['content_id' => 3, 'content_type' => LikesEnum::Comments->value, 'like_time' => 10, 'id_member' => 2]],
+				2,
+			],
+			'no likes found' => [
+				LikesEnum::Status,
+				[5, 6],
+				[],
+				2,
+			],
 		];
+	}
+
+	public function testIsContentAlreadyLikedTrue(): void
+	{
+		$this->dbClient->expects($this->once())
+			->method('query')
+			->willReturn('result');
+		$this->dbClient->expects($this->once())
+			->method('numRows')
+			->with('result')
+			->willReturn(1);
+		$this->dbClient->expects($this->once())
+			->method('freeResult');
+
+		$likeEntity = LikeEntity::from();
+		$likeEntity->setContentId(1);
+		$likeEntity->setIdMember(2);
+		$likeEntity->setContentType(LikesEnum::Status);
+
+		$result = $this->likeRepository->isContentAlreadyLiked($likeEntity);
+
+		$this->assertTrue($result);
 	}
 }
